b4b5940ee950f203a70b65982ca2cfc68762e7aa
Tim Sneath <timsneath@google.com>
2020-03-21 17:35:50 -0700

    Some Win32 function stubs

--- lib/win32.dart
+++ lib/win32.dart
@@ -1,4 +1,69 @@
 import 'dart:ffi';
 
 // LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-CreateWindowEx
+typedef windowProcNative = Int32 Function();
+typedef windowProcDart = int Function();
+
+// HWND CreateWindowExA(
+//   DWORD     dwExStyle,
+//   LPCSTR    lpClassName,
+//   LPCSTR    lpWindowName,
+//   DWORD     dwStyle,
+//   int       X,
+//   int       Y,
+//   int       nWidth,
+//   int       nHeight,
+//   HWND      hWndParent,
+//   HMENU     hMenu,
+//   HINSTANCE hInstance,
+//   LPVOID    lpParam
+// );
+typedef createWindowsExNative = Int32 Function(
+    Int32 dwExStyle,
+    Pointer<Int16> lpClassName,
+    Pointer<Int16> lpWindowName,
+    Int32 dwStyle,
+    Int32 X,
+    Int32 Y,
+    Int32 nWidth,
+    Int32 nHeight,
+    Int32 hWndParent,
+    Int32 hMenu,
+    Int32 hInstance,
+    Pointer<Void> lpParam);
+
+typedef createWindowsExDart = int Function(
+    int dwExStyle,
+    Pointer<Int16> lpClassName,
+    Pointer<Int16> lpWindowName,
+    int dwStyle,
+    int X,
+    int Y,
+    int nWidth,
+    int nHeight,
+    int hWndParent,
+    int hMenu,
+    int hInstance,
+    Pointer<Void> lpParam);
+
+// BOOL ShowWindow(
+//   HWND hWnd,
+//   int  nCmdShow
+// );
+typedef showWindowsNative = Int8 Function(Int32 hWnd, int nCmdShow);
+typedef showWindowsDart = int Function(int hWnd, int nCmdShow);
+
+class Win32 {
+  createWindowsExDart CreateWindowsEx;
+  showWindowsDart ShowWindow;
+
+  Win32() {
+    final user32 = DynamicLibrary.open('User32.dll');
+
+    CreateWindowsEx =
+        user32.lookupFunction<createWindowsExNative, createWindowsExDart>(
+            'CreateWindowsExA');
+    ShowWindow =
+        user32.lookupFunction<showWindowsNative, showWindowsDart>('ShowWindow');
+  }
+}
