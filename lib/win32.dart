bd0bf248fc27846be44965c0547c16dfd6121abf
Tim Sneath <timsneath@google.com>
2020-03-21 17:01:10 -0700

    Initial repo

---
+++ lib/win32.dart
@@ -0,0 +1,4 @@
+import 'dart:ffi';
+
+// LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+CreateWindowEx
---
+++ test/win32_test.dart
@@ -0,0 +1,8 @@
+import 'package:win32/win32.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
