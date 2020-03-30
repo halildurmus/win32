d690b54d56d463c9493974c15718dd262d3b960e
Tim Sneath <tim@sneath.org>
2020-03-29 17:35:36 -0700

    test tetris

--- example/tetris/test.dart
+++ example/tetris/test.dart
@@ -1,13 +1,25 @@
+import 'dart:io';
+
 import 'piece.dart';
 import 'pieceset.dart';
 import 'package:win32/win32.dart';
 
 void main() {
   final ps = PieceSet();
+  var hashes = [];
+  // print('constructed: ${ps.pieces[0][0]}');
 
   for (var i = 0; i < ps.pieces.length; i++) {
     for (var j = 0; j < ps.pieces[i].length; j++) {
-      print('piece[$i][$j]:\n${ps.pieces[i][j]}');
+      if (hashes.contains(ps.pieces[i][j].hashCode)) {
+        print('duplicate');
+        exit(1);
+      } else {
+        hashes.add(ps.pieces[i][j].hashCode);
+      }
+
+      print(ps.pieces[i][j]);
     }
   }
+  print(hashes.length);
 }
