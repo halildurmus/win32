7d618cb78b612deb9b7a119b56bf3f549aaccb1d
Tim Sneath <tim@sneath.org>
2020-03-30 17:07:25 -0700

    Fixed KnownFolder sample

--- example/knownfolder.dart
+++ example/knownfolder.dart
@@ -3,6 +3,7 @@
 // Shows usage of console APIs
 
 import 'dart:ffi';
+import 'dart:io';
 import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
@@ -14,7 +15,14 @@ void main() {
   final result =
       SHGetFolderPath(NULL, CSIDL_PERSONAL | CSIDL_FLAG_CREATE, NULL, 0, path);
   print(result.toString());
-//   Pointer<Int16> ptr = path.cast<Int16>();
-// for(int v = 0; v < MAX_PATH; v++)
-// {  print(path.elementAt(v).ref);
+
+  final ptr = Pointer<Uint16>.fromAddress(path.address);
+  for (var v = 0; v < MAX_PATH; v++) {
+    final charCode = ptr.elementAt(v).value;
+    if (charCode != 0) {
+      stdout.write(String.fromCharCode(charCode));
+    } else {
+      break;
+    }
+  }
 }
