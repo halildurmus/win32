ee503609d05a1c45ba46e96a3bf24cc8c61de862
Tim Sneath <tim@sneath.org>
2020-03-30 17:03:57 -0700

    trying to print path

--- example/knownfolder.dart
+++ example/knownfolder.dart
@@ -2,6 +2,7 @@
 
 // Shows usage of console APIs
 
+import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
@@ -13,4 +14,7 @@ void main() {
   final result =
       SHGetFolderPath(NULL, CSIDL_PERSONAL | CSIDL_FLAG_CREATE, NULL, 0, path);
   print(result.toString());
+//   Pointer<Int16> ptr = path.cast<Int16>();
+// for(int v = 0; v < MAX_PATH; v++)
+// {  print(path.elementAt(v).ref);
 }
