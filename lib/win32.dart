e1c0ae6804cdc38b2bc87704801c5907c078202b
Tim Sneath <tim@sneath.org>
2020-03-31 17:41:23 -0700

    hide shell32 and ole32

--- lib/win32.dart
+++ lib/win32.dart
@@ -3,4 +3,4 @@ library win32;
 export 'src/constants.dart';
 export 'src/macros.dart';
 export 'src/structs.dart';
-export 'src/win32.dart' hide user32, gdi32, kernel32;
+export 'src/win32.dart' hide user32, gdi32, kernel32, shell32, ole32;
