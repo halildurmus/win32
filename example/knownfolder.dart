fde454e1a6ed3553e13d06a6912594244e4dba06
Tim Sneath <tim@sneath.org>
2020-04-01 12:12:07 -0700

    align C++ and Dart KnownFolder

--- example/knownfolder.dart
+++ example/knownfolder.dart
@@ -38,32 +38,29 @@ void getFolderPath() {
 /// Get the path for a known Windows folder, using the modern API
 void getKnownFolderPath() {
   final guidFolder = GUID.fromString(FOLDERID_Documents);
-  assert(sizeOf<GUID>() == 16);
-  print('Looking for folder with ID: $guidFolder');
 
-  final path = allocate<Uint16>(count: MAX_PATH);
-  final ptr = Pointer<Void>.fromAddress(path.address);
+  final buffer = allocate<Uint16>(count: MAX_PATH);
+  final ptr = Pointer<Void>.fromAddress(buffer.address);
 
-  final result =
-      SHGetKnownFolderPath(guidFolder.addressOf, KF_FLAG_DEFAULT, 0, ptr);
+  final hr = SHGetKnownFolderPath(guidFolder.addressOf, 0, 0, ptr);
 
-  if (SUCCEEDED(result)) {
+  if (SUCCEEDED(hr)) {
     print('SHGetKnownFolderPath returned ${fromUtf16(ptr, MAX_PATH)}');
     CoTaskMemFree(ptr);
   } else {
-    if (result == E_FAIL) {
+    if (hr == E_FAIL) {
       print('SHGetKnownFolderPath returned error code E_FAIL');
-    } else if (result == E_INVALIDARG) {
+    } else if (hr == E_INVALIDARG) {
       print('SHGetKnownFolderPath returned error code E_INVALIDARG');
     } else {
       print('SHGetKnownFolderPath returned error code '
-          '0x${result.toUnsigned(32).toRadixString(16)}');
+          '0x${hr.toUnsigned(32).toRadixString(16)}');
     }
   }
 }
 
 void main() {
-  getFolderPath();
-  print('');
+  // getFolderPath();
+  // print('');
   getKnownFolderPath();
 }
