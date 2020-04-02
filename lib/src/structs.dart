26d147b993a5098a1eccee38b06f3e69521cc601
Tim Sneath <tim@sneath.org>
2020-04-01 18:15:44 -0700

    hacky but functional toString()

--- lib/src/structs.dart
+++ lib/src/structs.dart
@@ -401,7 +401,8 @@ class GUID extends Struct {
         guidString.substring(25, 27) +
         guidString.substring(22, 24) +
         guidString.substring(20, 22);
-    print(rawString);
+
+    // We need to split this to avoid overflowing a signed int.parse()
     guid.Data4 = (int.parse('${rawString.substring(0, 4)}', radix: 16) << 48) +
         int.parse('${rawString.substring(4, 16)}', radix: 16);
 
@@ -410,12 +411,26 @@ class GUID extends Struct {
 
   /// Print GUID in common {FDD39AD0-238F-46AF-ADB4-6C85480369C7} format
   @override
-  String toString() =>
-      '{${Data1.toRadixString(16).padLeft(8, '0').toUpperCase()}-'
-      '${Data2.toRadixString(16).padLeft(4, '0').toUpperCase()}-'
-      '${Data3.toRadixString(16).padLeft(4, '0').toUpperCase()}-'
-      '${((Data4 >> 48) & 0xFFFF).toRadixString(16).padLeft(4, '0').toUpperCase()}-'
-      '${(Data4 & 0xFFFFFFFFFFFF).toRadixString(16).padLeft(12, '0').toUpperCase()}}';
+  String toString() {
+    final comp1 = (Data4 & 0xFF).toRadixString(16) +
+        ((Data4 & 0xFF00) >> 8).toRadixString(16);
+
+    // This is hacky as all get-out :)
+    final comp2 = ((Data4 & 0xFF0000) >> 16).toRadixString(16).padLeft(2, '0') +
+        ((Data4 & 0xFF000000) >> 24).toRadixString(16).padLeft(2, '0') +
+        ((Data4 & 0xFF00000000) >> 32).toRadixString(16).padLeft(2, '0') +
+        ((Data4 & 0xFF0000000000) >> 40).toRadixString(16).padLeft(2, '0') +
+        ((Data4 & 0xFF000000000000) >> 48).toRadixString(16).padLeft(2, '0') +
+        (BigInt.from(Data4 & 0xFF00000000000000).toUnsigned(64) >> 56)
+            .toRadixString(16)
+            .padLeft(2, '0');
+
+    return '{${Data1.toRadixString(16).padLeft(8, '0').toUpperCase()}-'
+        '${Data2.toRadixString(16).padLeft(4, '0').toUpperCase()}-'
+        '${Data3.toRadixString(16).padLeft(4, '0').toUpperCase()}-'
+        '${comp1.toUpperCase()}-'
+        '${comp2.toUpperCase()}}';
+  }
 }
 
 // *** CONSOLE STRUCTS ***
