bcf8a6b5d77b86de836d6663abd10038831be466
Tim Sneath <timsneath@google.com>
2020-04-04 19:25:46 -0700

    QI attempt

--- example/qi.dart
+++ example/qi.dart
@@ -1,22 +1,25 @@
-// dialog.dart
+// qi.dart
 
-// Opens the File Open dialog box and shows results
+// QueryInterface
 
 import 'dart:ffi';
 
 import 'package:ffi/ffi.dart';
+import 'package:win32/src/typedefs.dart';
 import 'package:win32/win32.dart';
 
 final hInstance = GetModuleHandle(nullptr);
 
 typedef queryInterfaceNative = Int32 Function(
-    Pointer<Uint64> obj, Pointer<GUID> riid, Pointer<Void> ppvObject);
+    Pointer<Uint64> obj, Pointer<GUID> riid, Pointer<Uint64> ppvObject);
 typedef queryInterfaceDart = int Function(
-    Pointer<Uint64> obj, Pointer<GUID> riid, Pointer<Void> ppvObject);
+    Pointer<Uint64> obj, Pointer<GUID> riid, Pointer<Uint64> ppvObject);
 
 typedef IModalWindowShowNative = Int32 Function(
     Pointer<Uint64> obj, Int64 hwndOwner);
 typedef IModalWindowShowDart = int Function(Pointer<Uint64> obj, int hwndOwner);
+// typedef IModalWindowShowNative = Int32 Function(Int64 hwndOwner);
+// typedef IModalWindowShowDart = int Function(int hwndOwner);
 
 class IFileDialogVtbl extends Struct {
   Pointer<NativeFunction> QueryInterface;
@@ -46,6 +49,14 @@ class IFileDialogVtbl extends Struct {
   Pointer<NativeFunction> SetClientGuid;
   Pointer<NativeFunction> ClearClientData;
   Pointer<NativeFunction> SetFilter;
+
+  factory IFileDialogVtbl.allocate() => allocate<IFileDialogVtbl>().ref
+    ..QueryInterface = allocate<Uint64>().cast()
+    ..AddRef = allocate<Uint64>().cast()
+    ..Release = allocate<Uint64>().cast()
+    ..Show = allocate<Uint64>().cast()
+    ..SetFileTypes = allocate<Uint64>().cast()
+    ..SetFileTypeIndex = allocate<Uint64>().cast();
 }
 
 String pointerAsString(Pointer ptr) =>
@@ -81,31 +92,46 @@ void main() {
       nullptr, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
   if (SUCCEEDED(hr)) {
     // Address of pointer variable that receives the interface pointer
-    final pFileOpen = allocate<Uint64>();
+    final ppvObj = IFileDialogVtbl.allocate();
 
     hr = CoCreateInstance(
         GUID.fromString(CLSID_FileOpenDialog).addressOf,
         nullptr,
         CLSCTX_ALL,
         GUID.fromString(IID_IFileOpenDialog).addressOf,
-        pFileOpen);
+        ppvObj.addressOf.cast());
 
-    print('pFileOpen address: ${pointerAsString(pFileOpen)}');
+    print('ppvObj address       : ${pointerAsString(ppvObj.addressOf)}');
 
     if (SUCCEEDED(hr)) {
-      // *ppv contains the requested interface pointer.
-      final pVtable = Pointer<IFileDialogVtbl>.fromAddress(pFileOpen.value);
-      print('pVtable is at *pFileOpen: ${pointerAsString(pVtable)}');
+      // *pFileOpen contains the requested interface pointer.
+      print('pQueryInterface is at: ${pointerAsString(ppvObj.QueryInterface)}');
+      print('pAddRef is at        : ${pointerAsString(ppvObj.AddRef)}');
+      print('pRelease is at       : ${pointerAsString(ppvObj.Release)}');
+      print('pShow is at          : ${pointerAsString(ppvObj.Show)}');
+
+      final pShowVal =
+          Pointer<Uint64>.fromAddress(ppvObj.Show.cast<Uint64>().value);
+      print('pShowVal is at       : ${pointerAsString(pShowVal)}');
+
+      // final pQINative =
+      //     Pointer<NativeFunction<queryInterfaceNative>>.fromAddress(
+      //         pQI.address);
+      // print('pQINative is at: ${pointerAsString(pQINative)}');
+      // final fQIDart = pQINative.asFunction<queryInterfaceDart>();
+
+      // hr = fQIDart(
+      //     Pointer<Uint64>.fromAddress(pQI.address),
+      //     GUID.fromString(IID_IFileOpenDialog).addressOf,
+      //     Pointer<Uint64>.fromAddress(ppvObj.addressOf.address));
 
       final pShowNative =
-          Pointer<NativeFunction<IModalWindowShowNative>>.fromAddress(
-              pVtable.address + 8 * 3);
-      print('pShowNative is at: ${pointerAsString(pShowNative)}');
+          ppvObj.Show.cast<NativeFunction<IModalWindowShowNative>>();
 
       final fShowDart = pShowNative.asFunction<IModalWindowShowDart>();
 
-      hr = fShowDart(pFileOpen, NULL);
-      print('fShowDart returned $hr');
+      hr = fShowDart(ppvObj.addressOf.cast(), NULL);
+      print('qiFunc returned $hr');
     } else {
       COMError(hr, 'CoCreateInstance');
     }
