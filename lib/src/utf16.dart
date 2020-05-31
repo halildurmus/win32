09b90ceb4fbd3a487b959ff6d0a80e63499e7263
Tim Sneath <timsneath@google.com>
2020-05-30 20:56:40 -0700

    Clean up unpackString()

--- lib/src/utf16.dart
+++ lib/src/utf16.dart
@@ -4,22 +4,13 @@
 // of Utf16
 
 import 'dart:ffi';
+
 import 'package:ffi/ffi.dart';
 
 extension Utf16Conversion on Pointer<Utf16> {
   String unpackString(int maxLength) {
-    final buf = StringBuffer();
-    final ptr = Pointer<Uint16>.fromAddress(address);
-
-    for (var v = 0; v < maxLength; v++) {
-      final charCode = ptr.elementAt(v).value;
-      if (charCode != 0) {
-        buf.write(String.fromCharCode(charCode));
-      } else {
-        return buf.toString();
-      }
-    }
-    return buf.toString();
+    final pathData = cast<Uint16>().asTypedList(maxLength);
+    return String.fromCharCodes(pathData, 0, pathData.indexOf(0));
   }
 
   // Assumes an array of null-terminated strings, with the final
