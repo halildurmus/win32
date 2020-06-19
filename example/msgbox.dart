1374ec4d25e2e87c224479312b9ad62d0128aeee
Tim Sneath <timsneath@google.com>
2020-06-19 13:56:44 -0700

    Free text alloc

--- example/msgbox.dart
+++ example/msgbox.dart
@@ -4,33 +4,47 @@
 
 import 'dart:ffi';
 
+import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
 void showTaskDialog() {
-  // Requires a reference to comctl32.dll v6 in the manifest to work, so
-  // commenting out here.
+  final message =
+      TEXT('Some text that will be displayed in the task dialog goes here.');
+  final title = TEXT('Title message goes here');
+
+  // Requires a reference to comctl32.dll v6 in the manifest to work
   final hr = TaskDialog(
       NULL,
       NULL,
-      TEXT('Title message goes here'),
-      TEXT('Some text that will be displayed in the task dialog goes here.'),
+      title,
+      message,
       nullptr,
       TASKDIALOG_COMMON_BUTTON_FLAGS.TDCBF_OK_BUTTON,
       TD_INFORMATION_ICON,
       nullptr);
   print(hr);
+
+  free(title);
+  free(message);
 }
 
 void showMessageBox() {
+  final message =
+      TEXT('This is not really an error, but we are pretending for the sake '
+          'of this test.\n\nResource error.\nDo you want to try again?');
+  final title = TEXT('Dart MessageBox Test');
+
   final result = MessageBox(
       NULL,
-      TEXT('This is not really an error, but we are pretending for the sake '
-          'of this test.\n\nResource error.\nDo you want to try again?'),
-      TEXT('Dart MessageBox Test'),
+      message,
+      title,
       MB_ICONWARNING | // Warning
           MB_CANCELTRYCONTINUE | // Action button
           MB_DEFBUTTON2 // Second button is the default
       );
+
+  free(message);
+  free(title);
 
   switch (result) {
     case IDCANCEL:
@@ -47,5 +61,8 @@ void showMessageBox() {
 
 void main() {
   showMessageBox();
-  // showTaskDialog();
+
+  // Commenting out for general purposes, since this needs a Dart2Native
+  // executable with an app manifest to work.
+  showTaskDialog();
 }
