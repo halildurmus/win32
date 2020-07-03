4b07dc62c6ea52329175e31a8763e79f364280d8
Tim Sneath <timsneath@google.com>
2020-07-02 20:47:54 -0700

    add initPseudoConsole

--- example/conpty.dart
+++ example/conpty.dart
@@ -3,6 +3,29 @@ import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
+void initPseudoConsole() {
+  final inputReadSideHandle = allocate<IntPtr>();
+  final inputWriteSideHandle = allocate<IntPtr>();
+  final outputReadSideHandle = allocate<IntPtr>();
+  final outputWriteSideHandle = allocate<IntPtr>();
+
+  if (CreatePipe(inputReadSideHandle, inputWriteSideHandle, nullptr, 0) == 0) {
+    throw WindowsException(HRESULT_FROM_WIN32(GetLastError()));
+  }
+  if (CreatePipe(outputReadSideHandle, outputWriteSideHandle, nullptr, 0) ==
+      0) {
+    throw WindowsException(HRESULT_FROM_WIN32(GetLastError()));
+  }
+
+  final pseudoConsoleHandle = allocate<IntPtr>();
+  final size = (25 << 16) /* Y */ + 80; /* X */
+  final hr = CreatePseudoConsole(size, inputReadSideHandle.value,
+      outputWriteSideHandle.value, 0, pseudoConsoleHandle);
+  if (FAILED(hr)) {
+    throw WindowsException(hr);
+  }
+}
+
 void main() {
   final stdOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -10,4 +33,10 @@ void main() {
   GetConsoleMode(stdOut, consoleMode);
   var hr = SetConsoleMode(
       stdOut, consoleMode.value | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+  if (FAILED(hr)) {
+    throw WindowsException(hr);
+  }
+
+  initPseudoConsole();
+  print('all done');
 }
