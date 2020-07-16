f139980c6599dd888943863bd7f1a68493b566c4
Tim Sneath <timsneath@google.com>
2020-07-15 18:06:32 -0700

    Add a unit test for power management API

--- test/devices_test.dart
+++ test/devices_test.dart
@@ -17,5 +17,24 @@ void main() {
     final volume = volumeNamePtr.unpackString(MAX_PATH);
 
     expect(volume, startsWith('\\\\?\\'));
+
+    free(volumeNamePtr);
+  });
+
+  test('Power management API', () {
+    final powerStatus = SYSTEM_POWER_STATUS.allocate();
+
+    final result = GetSystemPowerStatus(powerStatus.addressOf);
+
+    // Sanity check results against some API provided ranges
+    final validBatteryPercentages = [for (var i = 0; i <= 100; i += 1) i]
+      ..add(255); // [0..100, 255]
+
+    expect(result, isNonZero);
+    expect(powerStatus.ACLineStatus, isIn([0, 1, 255]));
+    expect(powerStatus.SystemStatusFlag, isIn([0, 1]));
+    expect(powerStatus.BatteryLifePercent, isIn(validBatteryPercentages));
+
+    free(powerStatus.addressOf);
   });
 }
