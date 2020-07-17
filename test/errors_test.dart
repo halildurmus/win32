aaf9bc39334a1fa31b8ac6097fe022b6b9ab4620
Tim Sneath <timsneath@google.com>
2020-07-16 21:14:26 -0700

    Add some more error tests

--- test/errors_test.dart
+++ test/errors_test.dart
@@ -18,4 +18,30 @@ void main() {
     expect(WindowsException(CO_E_ALREADYINITIALIZED).toString(),
         endsWith('CoInitialize has already been called.'));
   });
+
+  test('HRESULT_FROM_WIN32 should give a valid result', () {
+    final hr = HRESULT_FROM_WIN32(ERROR_INVALID_HANDLE);
+    expect(WindowsException(hr).toString(), endsWith('The handle is invalid.'));
+  });
+
+  test('STATUS_SUCCESS should be a success after conversion to HRESULT', () {
+    final hr = HRESULT_FROM_WIN32(STATUS_SUCCESS);
+    expect(SUCCEEDED(hr), equals(true));
+    expect(FAILED(hr), equals(false));
+  });
+
+  test('S_OK succeeds', () {
+    expect(SUCCEEDED(S_OK), equals(true));
+    expect(FAILED(S_OK), equals(false));
+  });
+
+  test('S_FALSE succeeds', () {
+    expect(SUCCEEDED(S_FALSE), equals(true));
+    expect(FAILED(S_FALSE), equals(false));
+  });
+
+  test('E_FAIL fails', () {
+    expect(SUCCEEDED(E_FAIL), equals(false));
+    expect(FAILED(E_FAIL), equals(true));
+  });
 }
