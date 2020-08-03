9986cabe6a0609a57cafbcb7d8515567337fdf4d
Tim Sneath <timsneath@google.com>
2020-08-03 16:27:48 -0700

    Update Windows template

--- example/wallpaper/windows/runner/window_configuration.cpp
+++ example/wallpaper/windows/runner/window_configuration.cpp
@@ -1,6 +1,6 @@
 #include "window_configuration.h"
 
-const wchar_t* kFlutterWindowTitle = L"filepicker_windows_example";
+const wchar_t* kFlutterWindowTitle = L"wallpaper";
 const unsigned int kFlutterWindowOriginX = 10;
 const unsigned int kFlutterWindowOriginY = 10;
 const unsigned int kFlutterWindowWidth = 1280;
