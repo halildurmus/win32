46b8608a292fb18ae0eb133c4523f4d01c959099
Tim Sneath <timsneath@google.com>
2020-08-10 00:36:50 -0700

    Add Win32 to generate command

--- tool/generate.cmd
+++ tool/generate.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+echo Generating C-style Win32 APIs
+call dart %~dp0win32\typeread.dart 
+
 echo Generating COM classes
 call dart %~dp0generate\generate.dart %~dp0generate\com %~dp0..\lib\src\generated
 
@@ -10,4 +13,5 @@ echo Generating Windows Runtime classes from inspection
 call dart %~dp0winmd\winmd.dart %~dp0..\lib\src\generated
 
 echo Formatting generated source code
+call dart format %~dp0..\lib\src
 call dart format %~dp0..\lib\src\generated
