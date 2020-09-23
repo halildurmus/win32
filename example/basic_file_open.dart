555c4d0d8e69c03c4c27b47d963c5c8f753baeeb
Tim Sneath <timsneath@google.com>
2020-09-22 20:05:31 -0700

    Tidy OpenFilePicker syntax

--- example/basic_file_open.dart
+++ example/basic_file_open.dart
@@ -2,17 +2,16 @@ import 'package:filepicker_windows/filepicker_windows.dart';
 
 // Normal file open dialog box example
 void main() {
-  final file = OpenFilePicker();
-
-  file.filterSpecification = {
-    'Word Document (*.doc)': '*.doc',
-    'Web Page (*.htm; *.html)': '*.htm;*.html',
-    'Text Document (*.txt)': '*.txt',
-    'All Files': '*.*'
-  };
-  file.defaultFilterIndex = 0;
-  file.defaultExtension = 'doc';
-  file.title = 'Select a document';
+  final file = OpenFilePicker()
+    ..filterSpecification = {
+      'Word Document (*.doc)': '*.doc',
+      'Web Page (*.htm; *.html)': '*.htm;*.html',
+      'Text Document (*.txt)': '*.txt',
+      'All Files': '*.*'
+    }
+    ..defaultFilterIndex = 0
+    ..defaultExtension = 'doc'
+    ..title = 'Select a document';
 
   final result = file.getFile();
   if (result != null) {
