50ce57e95a0ee4d6e6a1e9f58ee43b350798a187
Tim Sneath <timsneath@google.com>
2020-11-19 13:53:32 -0800

    Fix in-progress sample for null safety

--- example/play_sound.dart
+++ example/play_sound.dart
@@ -6,9 +6,12 @@ import 'package:win32/win32.dart';
 void main() {
   print('Type the audio file path.');
   final file = stdin.readLineSync();
-  final result = PlaySound(Utf16.toUtf16(file), NULL, SND_FILENAME | SND_ASYNC);
-  print('Press any button to continue');
-  stdin
-      .readByteSync(); // This is needed since we are using the SND_ASNYC flag and the PlaySound function returns immediately, to avoid this behavior use SND_SYNC instead (default)
-  print('Result: $result');
+  if (file != null && File(file).existsSync()) {
+    final result =
+        PlaySound(Utf16.toUtf16(file), NULL, SND_FILENAME | SND_ASYNC);
+    print('Press any button to continue');
+    stdin
+        .readByteSync(); // This is needed since we are using the SND_ASNYC flag and the PlaySound function returns immediately, to avoid this behavior use SND_SYNC instead (default)
+    print('Result: $result');
+  }
 }
