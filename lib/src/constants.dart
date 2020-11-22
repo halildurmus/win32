7f40221c6d52ac72af3cb0eea268932821a4efc6
Tim Sneath <timsneath@google.com>
2020-11-22 12:34:09 -0800

    Add WS_EX_* constants

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -369,7 +369,7 @@ const WS_CAPTION = 0x00C00000;
 const WS_CHILD = 0x40000000;
 
 /// Same as the WS_CHILD style.
-const WS_CHILDWINDOW = 0x40000000;
+const WS_CHILDWINDOW = WS_CHILD;
 
 /// Excludes the area occupied by child windows when drawing occurs within the
 /// parent window. This style is used when creating the parent window.
@@ -410,7 +410,7 @@ const WS_GROUP = 0x00020000;
 const WS_HSCROLL = 0x00100000;
 
 /// The window is initially minimized. Same as the WS_MINIMIZE style.
-const WS_ICONIC = 0x20000000;
+const WS_ICONIC = WS_MINIMIZE;
 
 /// The window is initially maximized.
 const WS_MAXIMIZE = 0x01000000;
@@ -447,7 +447,7 @@ const WS_POPUP = 0x80000000;
 const WS_POPUPWINDOW = WS_POPUP | WS_BORDER | WS_SYSMENU;
 
 /// The window has a sizing border. Same as the WS_THICKFRAME style.
-const WS_SIZEBOX = 0x00040000;
+const WS_SIZEBOX = WS_THICKFRAME;
 
 /// The window has a window menu on its title bar. The WS_CAPTION style must
 /// also be specified.
@@ -470,15 +470,10 @@ const WS_THICKFRAME = 0x00040000;
 
 /// The window is an overlapped window. An overlapped window has a title bar and
 /// a border. Same as the WS_OVERLAPPED style.
-const WS_TILED = 0x00000000;
+const WS_TILED = WS_OVERLAPPED;
 
 /// The window is an overlapped window. Same as the WS_OVERLAPPEDWINDOW style.
-const WS_TILEDWINDOW = WS_OVERLAPPED |
-    WS_CAPTION |
-    WS_SYSMENU |
-    WS_THICKFRAME |
-    WS_MINIMIZEBOX |
-    WS_MAXIMIZEBOX;
+const WS_TILEDWINDOW = WS_OVERLAPPEDWINDOW;
 
 /// The window is initially visible.
 ///
@@ -488,6 +483,137 @@ const WS_VISIBLE = 0x10000000;
 
 /// The window has a vertical scroll bar.
 const WS_VSCROLL = 0x00200000;
+
+/// The window has a double border; the window can, optionally, be created with
+/// a title bar by specifying the WS_CAPTION style in the dwStyle parameter.
+const WS_EX_DLGMODALFRAME = 0x00000001;
+
+/// The child window created with this style does not send the WM_PARENTNOTIFY
+/// message to its parent window when it is created or destroyed.
+const WS_EX_NOPARENTNOTIFY = 0x00000004;
+
+/// The window should be placed above all non-topmost windows and should stay
+/// above them, even when the window is deactivated. To add or remove this
+/// style, use the SetWindowPos function.
+const WS_EX_TOPMOST = 0x00000008;
+
+/// The window accepts drag-drop files.
+const WS_EX_ACCEPTFILES = 0x00000010;
+
+/// The window should not be painted until siblings beneath the window (that
+/// were created by the same thread) have been painted. The window appears
+/// transparent because the bits of underlying sibling windows have already been
+/// painted.
+const WS_EX_TRANSPARENT = 0x00000020;
+
+/// The window is a MDI child window.
+const WS_EX_MDICHILD = 0x00000040;
+
+/// The window is intended to be used as a floating toolbar. A tool window has a
+/// title bar that is shorter than a normal title bar, and the window title is
+/// drawn using a smaller font. A tool window does not appear in the taskbar or
+/// in the dialog that appears when the user presses ALT+TAB. If a tool window
+/// has a system menu, its icon is not displayed on the title bar. However, you
+/// can display the system menu by right-clicking or by typing ALT+SPACE.
+const WS_EX_TOOLWINDOW = 0x00000080;
+
+/// The window has a border with a raised edge.
+const WS_EX_WINDOWEDGE = 0x00000100;
+
+/// The window has a border with a sunken edge.
+const WS_EX_CLIENTEDGE = 0x00000200;
+
+/// The title bar of the window includes a question mark.
+///
+/// When the user clicks the question mark, the cursor changes to a question
+/// mark with a pointer. If the user then clicks a child window, the child
+/// receives a WM_HELP message. The child window should pass the message to the
+/// parent window procedure, which should call the WinHelp function using the
+/// HELP_WM_HELP command. The Help application displays a pop-up window that
+/// typically contains help for the child window. WS_EX_CONTEXTHELP cannot be
+/// used with the WS_MAXIMIZEBOX or WS_MINIMIZEBOX styles.
+const WS_EX_CONTEXTHELP = 0x00000400;
+
+/// The window has generic "right-aligned" properties. This depends on the
+/// window class. This style has an effect only if the shell language is Hebrew,
+/// Arabic, or another language that supports reading-order alignment;
+/// otherwise, the style is ignored.
+const WS_EX_RIGHT = 0x00001000;
+
+/// The window has generic left-aligned properties. This is the default.
+const WS_EX_LEFT = 0x00000000;
+
+/// If the shell language is Hebrew, Arabic, or another language that supports
+/// reading-order alignment, the window text is displayed using right-to-left
+/// reading-order properties. For other languages, the style is ignored.
+const WS_EX_RTLREADING = 0x00002000;
+
+/// The window text is displayed using left-to-right reading-order properties.
+/// This is the default.
+const WS_EX_LTRREADING = 0x00000000;
+
+/// If the shell language is Hebrew, Arabic, or another language that supports
+/// reading order alignment, the vertical scroll bar (if present) is to the left
+/// of the client area. For other languages, the style is ignored.
+const WS_EX_LEFTSCROLLBAR = 0x00004000;
+
+/// The vertical scroll bar (if present) is to the right of the client area.
+/// This is the default.
+const WS_EX_RIGHTSCROLLBAR = 0x00000000;
+
+/// The window itself contains child windows that should take part in dialog box
+/// navigation.
+///
+/// If this style is specified, the dialog manager recurses into children of
+/// this window when performing navigation operations such as handling the TAB
+/// key, an arrow key, or a keyboard mnemonic.
+const WS_EX_CONTROLPARENT = 0x00010000;
+
+/// The window has a three-dimensional border style intended to be used for
+/// items that do not accept user input.
+const WS_EX_STATICEDGE = 0x00020000;
+
+/// Forces a top-level window onto the taskbar when the window is visible.
+const WS_EX_APPWINDOW = 0x00040000;
+
+/// The window is an overlapped window.
+const WS_EX_OVERLAPPEDWINDOW = WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE;
+
+/// The window is palette window, which is a modeless dialog box that presents
+/// an array of commands.
+const WS_EX_PALETTEWINDOW = WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW | WS_EX_TOPMOST;
+
+/// The window is a layered window. This style cannot be used if the window has
+/// a class style of either CS_OWNDC or CS_CLASSDC.
+const WS_EX_LAYERED = 0x00080000;
+
+/// The window does not pass its window layout to its child windows.
+const WS_EX_NOINHERITLAYOUT = 0x00100000;
+
+/// The window does not render to a redirection surface. This is for windows
+/// that do not have visible content or that use mechanisms other than surfaces
+/// to provide their visual.
+const WS_EX_NOREDIRECTIONBITMAP = 0x00200000;
+
+/// If the shell language is Hebrew, Arabic, or another language that supports
+/// reading order alignment, the horizontal origin of the window is on the right
+/// edge. Increasing horizontal values advance to the left.
+const WS_EX_LAYOUTRTL = 0x00400000;
+
+/// Paints all descendants of a window in bottom-to-top painting order using
+/// double-buffering.
+///
+/// Bottom-to-top painting order allows a descendent window to have translucency
+/// (alpha) and transparency (color-key) effects, but only if the descendent
+/// window also has the WS_EX_TRANSPARENT bit set. Double-buffering allows the
+/// window and its descendents to be painted without flicker. This cannot be
+/// used if the window has a class style of either CS_OWNDC or CS_CLASSDC.
+const WS_EX_COMPOSITED = 0x02000000;
+
+/// A top-level window created with this style does not become the foreground
+/// window when the user clicks it. The system does not bring this window to the
+/// foreground when the user minimizes or closes the foreground window.
+const WS_EX_NOACTIVATE = 0x08000000;
 
 // -----------------------------------------------------------------------------
 // WindowMessage constants
