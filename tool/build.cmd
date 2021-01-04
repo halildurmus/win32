fdedc264f6a23b703c4034836f5da117b4998f2e
Tim Sneath <timsneath@google.com>
2021-01-04 10:24:15 -0800

    Update compilation script

--- tool/build.cmd
+++ tool/build.cmd
@@ -1,6 +1,7 @@
 @echo off
-call dart2native example\console.dart -o example\bin\console.exe
-call dart2native example\hello.dart -o example\bin\hello.exe
-call dart2native example\paint.dart -o example\bin\paint.exe
-call dart2native example\tetris\main.dart -o example\bin\tetris.exe
-call dart2native example\msgbox.dart -o example\bin\msgbox.exe
+echo Compiling Dart executables...
+call dart compile exe example\console.dart -o example\bin\console.exe
+call dart compile exe example\hello.dart -o example\bin\hello.exe
+call dart compile exe example\notepad\notepad.dart -o example\bin\dartnote.exe
+call dart compile exe example\tetris\main.dart -o example\bin\tetris.exe
+call dart compile exe example\taskdialog.dart -o example\bin\taskdialog.exe
