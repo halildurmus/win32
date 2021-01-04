98f64d92108dfad1e182df2091e0a796e0723070
Tim Sneath <timsneath@google.com>
2021-01-04 11:50:10 -0800

    Add comment to neutrality_test.md

--- test/neutrality_test.dart
+++ test/neutrality_test.dart
@@ -2,6 +2,11 @@ import 'package:test/test.dart';
 
 import 'package:win32/win32.dart';
 
+// This test has two purposes:
+// 1. Make sure that the act of including win32 as a dependency does not cause
+//    failures on other platforms.
+// 2. Prevent CI from failing on other platforms (GitHub Actions fails unless at
+//    least one test is run successfully.)
 void main() {
   test('Dormant package does not cause failures on other platforms', () {
     final point = POINT.allocate();
