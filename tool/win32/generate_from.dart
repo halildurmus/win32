7cbacc1f8f0772b1135c5f6069344b6c672e1105
Tim Sneath <timsneath@google.com>
2021-02-14 18:16:09 -0800

    Update generated list

--- tool/win32/generate_from.dart
+++ tool/win32/generate_from.dart
@@ -6,13 +6,19 @@ const winmdGenerated = [
   'dxva2',
   'gdi32',
   'powrprof',
+  // 'psapi',
   'rometadata',
   // 'shcore',
+  // 'shell32',
   'version',
   'winmm'
 ];
+
+// bthprops is broken because of BLUETOOTH_DEVICE_INFO_STRUCT incorrect name
 
 // psapi is missing because of
 // https://github.com/microsoft/win32metadata/issues/229
 
 // shcore is missing because module is api-ms-win-shcore-scaling-l1-1-1
+
+// shell32 is missing because of SHGetKnownFolderPath (Dart test fails)
