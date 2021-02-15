553b034607331d0ab37ac7dd61ff63bbbb595fdd
Tim Sneath <timsneath@google.com>
2021-02-14 17:57:27 -0800

    Add winmm and powrprof

--- tool/win32/generate_from.dart
+++ tool/win32/generate_from.dart
@@ -1,1 +1,11 @@
-const winmdGenerated = ['gdi32', 'advapi32', 'comctl32', 'comdlg32'];
+const winmdGenerated = [
+  'gdi32',
+  'advapi32',
+  'comctl32',
+  'comdlg32',
+  'powrprof',
+  'winmm'
+];
+
+// psapi is missing because of
+// https://github.com/microsoft/win32metadata/issues/229
