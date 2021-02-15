a915571be923de7c608af9d9fc34cac7da7c3130
Tim Sneath <timsneath@google.com>
2021-02-14 18:02:04 -0800

    Add dxva2

--- tool/win32/generate_from.dart
+++ tool/win32/generate_from.dart
@@ -1,11 +1,17 @@
 const winmdGenerated = [
-  'gdi32',
   'advapi32',
+  // 'bthprops',
   'comctl32',
   'comdlg32',
+  'dxva2',
+  'gdi32',
   'powrprof',
+  'rometadata',
+  // 'shcore',
   'winmm'
 ];
 
 // psapi is missing because of
 // https://github.com/microsoft/win32metadata/issues/229
+
+// shcore is missing because module is api-ms-win-shcore-scaling-l1-1-1
