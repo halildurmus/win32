8ea35ec97a82bf93f530d61e01394fa1260c8160
Tim Sneath <timsneath@google.com>
2021-02-28 21:28:35 -0800

    Tidy up and restructure

--- tool/metadata/win32_enums.dart
+++ tool/metadata/win32_enums.dart
@@ -1,8 +1,7 @@
 import 'dart:io';
 
-import 'package:winmd/winmd.dart';
-
 import 'package:win32/win32.dart';
+import 'package:winmd/winmd.dart';
 
 List<String> namespaces = [];
 
@@ -17,7 +16,40 @@ void initNamespaces(Scope scope) {
       namespaceSet.add('Windows.Win32.$namespace');
     }
   }
-  namespaces = namespaceSet.toList();
+  namespaces = namespaceSet.toList()..sort((a, b) => a.compareTo(b));
+}
+
+String processEnumeration(Enumeration enumClass) {
+  final buffer = StringBuffer();
+
+  // Get the enum name
+  var dartClass = enumClass.typeName.split('.').last;
+
+  // Dart treats types beginning with _ as private to the library
+  while (dartClass.startsWith('_')) {
+    dartClass = dartClass.substring(1);
+  }
+
+  // Some weirdness in Win32 metadata, see:
+  // https://github.com/microsoft/win32metadata/issues/297
+  if (dartClass.startsWith('MIDL___')) {
+    return '';
+  }
+
+  buffer.writeln('/// {@category Enum}');
+  buffer.writeln('class $dartClass {');
+
+  // The first field is always the special field _value
+  for (final field in enumClass.fields.keys.skip(1)) {
+    if (field != null && field.startsWith('_')) continue;
+
+    final value = enumClass.fields[field]!.toHexString(32);
+    buffer.writeln('  static const ${field} = $value;');
+  }
+
+  buffer.writeln('}\n');
+
+  return buffer.toString();
 }
 
 void main() {
@@ -37,36 +69,11 @@ void main() {
           File('lib/src/$folderName/enums.dart').openSync(mode: FileMode.write);
 
       final enumsLength = filteredEnums.length;
-      print('Processing $enumsLength enums in $namespace.');
+      print('Processing $enumsLength enums in $namespace');
       final buffer = StringBuffer();
 
       for (final enumClass in filteredEnums) {
-        // Get the enum name
-        var dartClass = enumClass.typeName.split('.').last;
-
-        // Dart treats types beginning with _ as private to the library
-        while (dartClass.startsWith('_')) {
-          dartClass = dartClass.substring(1);
-        }
-
-        // Some weirdness in Win32 metadata, see:
-        // https://github.com/microsoft/win32metadata/issues/297
-        if (dartClass.startsWith('MIDL___')) {
-          continue;
-        }
-
-        buffer.writeln('/// {@category Enum}');
-        buffer.writeln('class $dartClass {');
-
-        // The first field is always the special field _value
-        for (final field in enumClass.fields.keys.skip(1)) {
-          if (field != null && field.startsWith('_')) continue;
-
-          final value = enumClass.fields[field]!.toHexString(32);
-          buffer.writeln('  static const ${field} = $value;');
-        }
-
-        buffer.writeln('}\n');
+        buffer.write(processEnumeration(enumClass));
       }
       writer.writeStringSync(buffer.toString());
     }
