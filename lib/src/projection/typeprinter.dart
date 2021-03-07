cfd5ede0d06bc42ad854c48bc646511a9780e016
Tim Sneath <timsneath@google.com>
2021-03-07 11:40:40 -0800

    Add test printing

--- lib/src/projection/typeprinter.dart
+++ lib/src/projection/typeprinter.dart
@@ -267,6 +267,53 @@ class ${type.className} extends $interfaceWithoutNamespace {
     }
   }
 
+  static String classTestsAsString(ClassProjection projection) {
+    final interfaceName = projection.shortName;
+    final dartClassName = interfaceName.substring(1).toLowerCase();
+
+    final buffer = StringBuffer();
+    buffer.write('''
+// Copyright (c) 2020, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Tests that Win32 API prototypes can be successfully loaded (i.e. that
+// lookupFunction works for all the APIs generated)
+
+// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
+
+@TestOn('windows')
+
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+import 'package:test/test.dart';
+
+import 'package:win32/win32.dart';
+
+void main() {
+  final ptr = calloc<COMObject>();
+
+  final $dartClassName = $interfaceName(ptr);
+''');
+
+    for (final method in projection.methods) {
+      if (!method.isGetProperty && !method.isSetProperty) {
+        buffer.write('''
+        test('Can instantiate $interfaceName.${method.name}', () {
+          expect($dartClassName.${method.name}, isA<Function>());
+          });
+          ''');
+      }
+    }
+
+    buffer.write('''
+  free(ptr);
+  }''');
+
+    return buffer.toString();
+  }
+
   static String printType(TypeDef typeDef) {
     final type = ClassProjector(typeDef).projection;
     return headerAsString(type) +
@@ -282,4 +329,7 @@ class ${type.className} extends $interfaceWithoutNamespace {
       typedefsAsString(projection) +
       interfaceAsString(projection) +
       classAsString(projection);
+
+  static String printTests(ClassProjection projection) =>
+      classTestsAsString(projection);
 }
