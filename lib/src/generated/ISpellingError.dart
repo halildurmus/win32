6fe3d03dc5a50c7a8f488260190311997fc6264e
Tim Sneath <timsneath@google.com>
2021-03-12 13:55:15 -0800

    Regenerate COM files w/ winmd 0.4.4

--- lib/src/generated/ISpellingError.dart
+++ lib/src/generated/ISpellingError.dart
@@ -32,9 +32,9 @@ typedef _get_Length_Native = Int32 Function(Pointer obj, Pointer<Uint32> value);
 typedef _get_Length_Dart = int Function(Pointer obj, Pointer<Uint32> value);
 
 typedef _get_CorrectiveAction_Native = Int32 Function(
-    Pointer obj, Pointer<Int32> value);
+    Pointer obj, Pointer<Uint32> value);
 typedef _get_CorrectiveAction_Dart = int Function(
-    Pointer obj, Pointer<Int32> value);
+    Pointer obj, Pointer<Uint32> value);
 
 typedef _get_Replacement_Native = Int32 Function(
     Pointer obj, Pointer<Pointer<Utf16>> value);
@@ -75,7 +75,7 @@ class ISpellingError extends IUnknown {
   }
 
   int get CorrectiveAction {
-    final retValuePtr = calloc<Int32>();
+    final retValuePtr = calloc<Uint32>();
 
     final hr =
         Pointer<NativeFunction<_get_CorrectiveAction_Native>>.fromAddress(
