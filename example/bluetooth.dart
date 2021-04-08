3a8f7e2f354843c17f1e4ae879b22a3588bfc9b1
Tim Sneath <timsneath@google.com>
2021-04-07 21:59:27 -0700

    Update bluetooth example

--- example/bluetooth.dart
+++ example/bluetooth.dart
@@ -10,10 +10,55 @@ import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
-String toHex(int value32) =>
-    '0x${value32.toUnsigned(32).toRadixString(16).padLeft(8, '0')}';
+String addressAsString(List<int> bluetoothAddress) => bluetoothAddress
+    .map((byte) => byte.toRadixString(16).toUpperCase().padLeft(2, '0'))
+    .join(':');
 
-void findBluetoothDevices(int btRadioHandle) {
+List<int> findBluetoothRadios() {
+  final findRadioParams = calloc<BLUETOOTH_FIND_RADIO_PARAMS>()
+    ..ref.dwSize = sizeOf<BLUETOOTH_FIND_RADIO_PARAMS>();
+
+  final radioHandles = <int>[];
+
+  final hRadio = calloc<IntPtr>();
+
+  try {
+    final hEnum = BluetoothFindFirstRadio(findRadioParams, hRadio);
+    if (hEnum == NULL) {
+      print('No radios found.');
+    } else {
+      radioHandles.add(hRadio.value);
+      print('Found a radio with handle: ${hRadio.value.toHexString(32)}');
+
+      while (BluetoothFindNextRadio(hEnum, hRadio) == TRUE) {
+        radioHandles.add(hRadio.value);
+      }
+      BluetoothFindRadioClose(hEnum);
+    }
+    return radioHandles;
+  } finally {
+    free(findRadioParams);
+    free(hRadio);
+  }
+}
+
+void findRadioInfo(int hRadio) {
+  final radioInfo = calloc<BLUETOOTH_RADIO_INFO>()
+    ..ref.dwSize = sizeOf<BLUETOOTH_RADIO_INFO>();
+
+  try {
+    final res = BluetoothGetRadioInfo(hRadio, radioInfo);
+    if (res == ERROR_SUCCESS) {
+      final bluetoothAddress = addressAsString(radioInfo.ref.address.rgBytes);
+      print('Radio name: ${radioInfo.ref.szName}');
+      print('Radio address: $bluetoothAddress');
+    }
+  } finally {
+    free(radioInfo);
+  }
+}
+
+void findBluetoothDevices() {
   final params = calloc<BLUETOOTH_DEVICE_SEARCH_PARAMS>()
     ..ref.dwSize = sizeOf<BLUETOOTH_DEVICE_SEARCH_PARAMS>();
   final info = calloc<BLUETOOTH_DEVICE_INFO>();
@@ -22,7 +67,10 @@ void findBluetoothDevices(int btRadioHandle) {
     final firstDeviceHandle = BluetoothFindFirstDevice(params, info);
 
     if (firstDeviceHandle != NULL) {
-      print(info.ref.szName);
+      print('Found device: ${info.ref.szName}');
+      while (BluetoothFindNextDevice(firstDeviceHandle, info) == TRUE) {
+        print('Found device: ${info.ref.szName}');
+      }
       BluetoothFindDeviceClose(firstDeviceHandle);
     } else {
       print('No devices found.');
@@ -34,31 +82,17 @@ void findBluetoothDevices(int btRadioHandle) {
 }
 
 void main() {
-  final findRadioParams = calloc<BLUETOOTH_FIND_RADIO_PARAMS>()
-    ..ref.dwSize = sizeOf<BLUETOOTH_FIND_RADIO_PARAMS>();
-  final radioInfo = calloc<BLUETOOTH_RADIO_INFO>()
-    ..ref.dwSize = sizeOf<BLUETOOTH_RADIO_INFO>();
-  final hRadio = calloc<IntPtr>();
+  final radioHandles = findBluetoothRadios();
 
-  try {
-    final hEnum = BluetoothFindFirstRadio(findRadioParams, hRadio);
-    if (hEnum != NULL) {
-      print('Found a radio with handle: ${toHex(hRadio.value)}');
+  for (final hRadio in radioHandles) {
+    findRadioInfo(hRadio);
 
-      final res = BluetoothGetRadioInfo(hRadio.value, radioInfo);
-      if (res == ERROR_SUCCESS) {
-        print('Got radio info.\n');
-        print('Radio name: ${radioInfo.ref.szName}');
-      }
-
-      findBluetoothDevices(hRadio.value);
-      BluetoothFindRadioClose(hRadio.value);
+    if (BluetoothIsDiscoverable(hRadio) == TRUE) {
+      print('Bluetooth radio is discoverable.');
     } else {
-      print('No Bluetooth radios found.');
+      print('Bluetooth radio is not discoverable.');
     }
-  } finally {
-    free(findRadioParams);
-    free(radioInfo);
-    free(hRadio);
   }
+
+  findBluetoothDevices();
 }
