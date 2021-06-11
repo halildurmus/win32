73fdb2d47e47fa5d3c1439cd518342ac6a4e6de1
Sunbreak <sunbreak.wang@gmail.com>
2021-06-11 16:53:11 +0800

    Add IToastNotification
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

---
+++ lib/src/com/IToastNotification.dart
@@ -0,0 +1,156 @@
+// IToastNotification.dart
+
+// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
+
+// ignore_for_file: unused_import
+
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+
+import '../combase.dart';
+import '../constants.dart';
+import '../exceptions.dart';
+import '../macros.dart';
+import '../ole32.dart';
+import '../structs.dart';
+import '../structs.g.dart';
+import '../utils.dart';
+
+import '../winrt/winrt_constants.dart';
+
+import 'IInspectable.dart';
+
+/// @nodoc
+const IID_IToastNotification = '{997E2675-059E-4E60-8B06-1760917C8B80}';
+
+typedef _get_Content_Native = Int32 Function(
+    Pointer obj, Pointer<Pointer> value);
+typedef _get_Content_Dart = int Function(Pointer obj, Pointer<Pointer> value);
+
+typedef _put_ExpirationTime_Native = Int32 Function(Pointer obj, Uint32 value);
+typedef _put_ExpirationTime_Dart = int Function(Pointer obj, int value);
+
+typedef _get_ExpirationTime_Native = Int32 Function(
+    Pointer obj, Pointer<Uint32> value);
+typedef _get_ExpirationTime_Dart = int Function(
+    Pointer obj, Pointer<Uint32> value);
+
+typedef _add_Dismissed_Native = Int32 Function(
+    Pointer obj, Pointer handler, Pointer<Uint32> result);
+typedef _add_Dismissed_Dart = int Function(
+    Pointer obj, Pointer handler, Pointer<Uint32> result);
+
+typedef _remove_Dismissed_Native = Int32 Function(Pointer obj, Uint32 token);
+typedef _remove_Dismissed_Dart = int Function(Pointer obj, int token);
+
+typedef _add_Activated_Native = Int32 Function(
+    Pointer obj, Pointer handler, Pointer<Uint32> result);
+typedef _add_Activated_Dart = int Function(
+    Pointer obj, Pointer handler, Pointer<Uint32> result);
+
+typedef _remove_Activated_Native = Int32 Function(Pointer obj, Uint32 token);
+typedef _remove_Activated_Dart = int Function(Pointer obj, int token);
+
+typedef _add_Failed_Native = Int32 Function(
+    Pointer obj, Pointer handler, Pointer<Uint32> result);
+typedef _add_Failed_Dart = int Function(
+    Pointer obj, Pointer handler, Pointer<Uint32> result);
+
+typedef _remove_Failed_Native = Int32 Function(Pointer obj, Uint32 token);
+typedef _remove_Failed_Dart = int Function(Pointer obj, int token);
+
+/// {@category Interface}
+/// {@category winrt}
+class IToastNotification extends IInspectable {
+  // vtable begins at 6, ends at 14
+
+  IToastNotification(Pointer<COMObject> ptr) : super(ptr);
+
+  Pointer get Content {
+    final retValuePtr = calloc<Pointer>();
+
+    try {
+      final hr = ptr.ref.lpVtbl.value
+          .elementAt(6)
+          .cast<Pointer<NativeFunction<_get_Content_Native>>>()
+          .value
+          .asFunction<_get_Content_Dart>()(ptr.ref.lpVtbl, retValuePtr);
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      final retValue = retValuePtr.value;
+      return retValue;
+    } finally {
+      free(retValuePtr);
+    }
+  }
+
+  set ExpirationTime(int value) {
+    final hr = ptr.ref.lpVtbl.value
+        .elementAt(7)
+        .cast<Pointer<NativeFunction<_put_ExpirationTime_Native>>>()
+        .value
+        .asFunction<_put_ExpirationTime_Dart>()(ptr.ref.lpVtbl, value);
+
+    if (FAILED(hr)) throw WindowsException(hr);
+  }
+
+  int get ExpirationTime {
+    final retValuePtr = calloc<Uint32>();
+
+    try {
+      final hr = ptr.ref.lpVtbl.value
+          .elementAt(8)
+          .cast<Pointer<NativeFunction<_get_ExpirationTime_Native>>>()
+          .value
+          .asFunction<_get_ExpirationTime_Dart>()(ptr.ref.lpVtbl, retValuePtr);
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      final retValue = retValuePtr.value;
+      return retValue;
+    } finally {
+      free(retValuePtr);
+    }
+  }
+
+  int add_Dismissed(Pointer handler, Pointer<Uint32> result) =>
+      ptr.ref.lpVtbl.value
+          .elementAt(9)
+          .cast<Pointer<NativeFunction<_add_Dismissed_Native>>>()
+          .value
+          .asFunction<_add_Dismissed_Dart>()(ptr.ref.lpVtbl, handler, result);
+
+  int remove_Dismissed(int token) => ptr.ref.lpVtbl.value
+      .elementAt(10)
+      .cast<Pointer<NativeFunction<_remove_Dismissed_Native>>>()
+      .value
+      .asFunction<_remove_Dismissed_Dart>()(ptr.ref.lpVtbl, token);
+
+  int add_Activated(Pointer handler, Pointer<Uint32> result) =>
+      ptr.ref.lpVtbl.value
+          .elementAt(11)
+          .cast<Pointer<NativeFunction<_add_Activated_Native>>>()
+          .value
+          .asFunction<_add_Activated_Dart>()(ptr.ref.lpVtbl, handler, result);
+
+  int remove_Activated(int token) => ptr.ref.lpVtbl.value
+      .elementAt(12)
+      .cast<Pointer<NativeFunction<_remove_Activated_Native>>>()
+      .value
+      .asFunction<_remove_Activated_Dart>()(ptr.ref.lpVtbl, token);
+
+  int add_Failed(Pointer handler, Pointer<Uint32> result) =>
+      ptr.ref.lpVtbl.value
+          .elementAt(13)
+          .cast<Pointer<NativeFunction<_add_Failed_Native>>>()
+          .value
+          .asFunction<_add_Failed_Dart>()(ptr.ref.lpVtbl, handler, result);
+
+  int remove_Failed(int token) => ptr.ref.lpVtbl.value
+      .elementAt(14)
+      .cast<Pointer<NativeFunction<_remove_Failed_Native>>>()
+      .value
+      .asFunction<_remove_Failed_Dart>()(ptr.ref.lpVtbl, token);
+}
