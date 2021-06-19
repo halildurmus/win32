a2d07dcf3e704fe9ef55e2f3bc1e74a6edf47e3f
Tim Sneath <tim@sneath.org>
2021-06-19 09:32:41 -0700

    Add more explanation for importing winsock

--- lib/winsock2.dart
+++ lib/winsock2.dart
@@ -8,9 +8,18 @@
 /// Windows operating systems.
 ///
 /// This library extends the core `win32` library with Windows Socket functions.
-/// Since this library is somewhat specialist and many of its function names are
-/// likely to conflict with other libraries or programs, it is separately
-/// exported.
+/// Since this library is somewhat specialist and many of its function names
+/// (e.g. `send`, `connect`) are likely to conflict with other libraries or
+/// programs, it is separately exported.
+///
+/// To add Winsock2 support to your app, use the additional line below:
+///
+/// ```dart
+/// import 'package:win32/win32.dart';
+/// import 'package:win32/winsock2.dart' as winsock2;
+/// ```
+///
+/// Now you can call, for instance, `winsock2.connect()` or `winsock2.send()`.
 ///
 /// The `winsock2` library will normally be used in conjunction with the main
 /// `win32` library, and the latter will be required for constants and structs
