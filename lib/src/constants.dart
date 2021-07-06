6ee35d220760d65e279fd2a58516911b71d8e292
Tim Sneath <tim@sneath.org>
2021-07-05 22:35:55 -0700

    Add DWM constants

--- lib/src/constants.dart
+++ lib/src/constants.dart
@@ -7151,6 +7151,127 @@ const SYMFLAG_METADATA = 0x00020000;
 const SYMFLAG_CLR_TOKEN = 0x00040000;
 
 // -----------------------------------------------------------------------------
+// DWM constants
+// -----------------------------------------------------------------------------
+
+/// Flags used by the DwmGetWindowAttribute and DwmSetWindowAttribute functions
+/// to specify window attributes for Desktop Window Manager (DWM) non-client
+/// rendering.
+class DWMWINDOWATTRIBUTE {
+  /// Use with DwmGetWindowAttribute. Discovers whether non-client rendering is
+  /// enabled. The retrieved value is of type BOOL. TRUE if non-client rendering
+  /// is enabled; otherwise, FALSE.
+  static const DWMWA_NCRENDERING_ENABLED = 1;
+
+  /// Use with DwmSetWindowAttribute. Sets the non-client rendering policy. The
+  /// pvAttribute parameter points to a value from the DWMNCRENDERINGPOLICY
+  /// enumeration.
+  static const DWMWA_NCRENDERING_POLICY = 2;
+
+  /// Use with DwmSetWindowAttribute. Enables or forcibly disables DWM
+  /// transitions. The pvAttribute parameter points to a value of type BOOL.
+  /// TRUE to disable transitions, or FALSE to enable transitions.
+  static const DWMWA_TRANSITIONS_FORCEDISABLED = 3;
+
+  /// Use with DwmSetWindowAttribute. Enables content rendered in the non-client
+  /// area to be visible on the frame drawn by DWM. The pvAttribute parameter
+  /// points to a value of type BOOL. TRUE to enable content rendered in the
+  /// non-client area to be visible on the frame; otherwise, FALSE.
+  static const DWMWA_ALLOW_NCPAINT = 4;
+
+  /// Use with DwmGetWindowAttribute. Retrieves the bounds of the caption button
+  /// area in the window-relative space. The retrieved value is of type RECT. If
+  /// the window is minimized or otherwise not visible to the user, then the
+  /// value of the RECT retrieved is undefined. You should check whether the
+  /// retrieved RECT contains a boundary that you can work with, and if it
+  /// doesn't then you can conclude that the window is minimized or otherwise
+  /// not visible.
+  static const DWMWA_CAPTION_BUTTON_BOUNDS = 5;
+
+  /// Use with DwmSetWindowAttribute. Specifies whether non-client content is
+  /// right-to-left (RTL) mirrored. The pvAttribute parameter points to a value
+  /// of type BOOL. TRUE if the non-client content is right-to-left (RTL)
+  /// mirrored; otherwise, FALSE.
+  static const DWMWA_NONCLIENT_RTL_LAYOUT = 6;
+
+  /// Use with DwmSetWindowAttribute. Forces the window to display an iconic
+  /// thumbnail or peek representation (a static bitmap), even if a live or
+  /// snapshot representation of the window is available. This value is normally
+  /// set during a window's creation, and not changed throughout the window's
+  /// lifetime. Some scenarios, however, might require the value to change over
+  /// time. The pvAttribute parameter points to a value of type BOOL. TRUE to
+  /// require a iconic thumbnail or peek representation; otherwise, FALSE.
+  static const DWMWA_FORCE_ICONIC_REPRESENTATION = 7;
+
+  /// Use with DwmSetWindowAttribute. Sets how Flip3D treats the window. The
+  /// pvAttribute parameter points to a value from the DWMFLIP3DWINDOWPOLICY
+  /// enumeration.
+  static const DWMWA_FLIP3D_POLICY = 8;
+
+  /// Use with DwmGetWindowAttribute. Retrieves the extended frame bounds
+  /// rectangle in screen space. The retrieved value is of type RECT.
+  static const DWMWA_EXTENDED_FRAME_BOUNDS = 9;
+
+  /// Use with DwmSetWindowAttribute. The window will provide a bitmap for use
+  /// by DWM as an iconic thumbnail or peek representation (a static bitmap) for
+  /// the window. DWMWA_HAS_ICONIC_BITMAP can be specified with
+  /// DWMWA_FORCE_ICONIC_REPRESENTATION. DWMWA_HAS_ICONIC_BITMAP normally is set
+  /// during a window's creation and not changed throughout the window's
+  /// lifetime. Some scenarios, however, might require the value to change over
+  /// time. The pvAttribute parameter points to a value of type BOOL. TRUE to
+  /// inform DWM that the window will provide an iconic thumbnail or peek
+  /// representation; otherwise, FALSE.
+  static const DWMWA_HAS_ICONIC_BITMAP = 10;
+
+  /// Use with DwmSetWindowAttribute. Do not show peek preview for the window.
+  /// The peek view shows a full-sized preview of the window when the mouse
+  /// hovers over the window's thumbnail in the taskbar. If this attribute is
+  /// set, hovering the mouse pointer over the window's thumbnail dismisses peek
+  /// (in case another window in the group has a peek preview showing). The
+  /// pvAttribute parameter points to a value of type BOOL. TRUE to prevent peek
+  /// functionality, or FALSE to allow it.
+  static const DWMWA_DISALLOW_PEEK = 11;
+
+  /// Use with DwmSetWindowAttribute. Prevents a window from fading to a glass
+  /// sheet when peek is invoked. The pvAttribute parameter points to a value of
+  /// type BOOL. TRUE to prevent the window from fading during another window's
+  /// peek, or FALSE for normal behavior.
+  static const DWMWA_EXCLUDED_FROM_PEEK = 12;
+
+  /// Use with DwmSetWindowAttribute. Cloaks the window such that it is not
+  /// visible to the user. The window is still composed by DWM.
+  static const DWMWA_CLOAK = 13;
+
+  /// Use with DwmGetWindowAttribute. If the window is cloaked, provides one of
+  /// the following values explaining why.
+  ///
+  /// - DWM_CLOAKED_APP (value 0x0000001). The window was cloaked by its owner
+  ///   application.
+  /// - DWM_CLOAKED_SHELL (value 0x0000002). The window was cloaked by the
+  ///   Shell.
+  /// - DWM_CLOAKED_INHERITED (value 0x0000004). The cloak value was inherited
+  ///   from its owner window.
+  static const DWMWA_CLOAKED = 14;
+
+  /// Use with DwmSetWindowAttribute. Freeze the window's thumbnail image with
+  /// its current visuals. Do no further live updates on the thumbnail image to
+  /// match the window's contents.
+  static const DWMWA_FREEZE_REPRESENTATION = 15;
+
+  /// [Windows 11 and above.] Use with DwmSetWindowAttribute. Enables a non-UWP
+  /// window to use host backdrop brushes. If this flag is set, then a Win32 app
+  /// that calls Windows::UI::Composition APIs can build transparency effects
+  /// using the host backdrop brush (see Compositor.CreateHostBackdropBrush).
+  /// The retrieved value is of type BOOL. TRUE to enable host backdrop brushes
+  /// for the window; otherwise, FALSE.
+  static const DWMWA_USE_HOSTBACKDROPBRUSH = 17;
+
+  /// The maximum recognized DWMWINDOWATTRIBUTE value, used for validation
+  /// purposes.
+  static const DWMWA_LAST = 17;
+}
+
+// -----------------------------------------------------------------------------
 // Token information constants
 // -----------------------------------------------------------------------------
 
