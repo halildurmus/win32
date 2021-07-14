5512da0072682c106901406e7460792000419900
Tim Sneath <tim@sneath.org>
2021-07-13 23:16:59 -0700

    Tidy tidy tidy

--- lib/src/console/structs.g.dart
+++ lib/src/console/structs.g.dart
@@ -6,43 +6,17 @@
 
 // THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
 
-// -----------------------------------------------------------------------------
-// Linter exceptions
-// -----------------------------------------------------------------------------
-// ignore_for_file: camel_case_types
-// ignore_for_file: camel_case_extensions
-//
-// Why? The linter defaults to throw a warning for types not named as camel
-// case. We deliberately break this convention to match the Win32 underlying
-// types.
-//
-//
-// ignore_for_file: unused_field
-//
-// Why? The linter complains about unused fields (e.g. a class that contains
-// underscore-prefixed members that are not used in the library. In this class,
-// we use this feature to ensure that sizeOf<STRUCT_NAME> returns a size at
-// least as large as the underlying native struct. See, for example,
-// ENUMLOGFONTEX.
-//
-//
-// ignore_for_file: unnecessary_getters_setters
-//
-// Why? In structs like VARIANT, we're using getters and setters to project the
-// same underlying data property to various union types. The trivial overhead is
-// outweighed by readability.
-//
-//
-// ignore_for_file: unused_import
-//
-// Why? We speculatively include some imports that we might generate a
-// requirement for.
-// -----------------------------------------------------------------------------
+// ignore_for_file: camel_case_extensions, camel_case_types
+// ignore_for_file: directives_ordering, unnecessary_getters_setters
+// ignore_for_file: unused_field, unused_import
 
 import 'dart:ffi';
 import 'dart:typed_data';
 
 import 'package:ffi/ffi.dart';
+
+import '../foundation/structs.g.dart';
+import '../console/structs.g.dart';
 
 class CHAR_INFO extends Struct {
   @Uint32()
