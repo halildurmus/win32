3397f3db937bca570bc493edd0b1e1215697abe1
Tim Sneath <tim@sneath.org>
2021-07-13 18:23:06 -0700

    Add finished func file

--- lib/src/foundation/apis.dart
+++ lib/src/foundation/apis.dart
@@ -14,81 +14,77 @@ import 'package:ffi/ffi.dart';
 
 import '../callbacks.dart';
 import '../combase.dart';
+// import 'enums.dart';
+// import 'structs.dart';
 
 final _oleaut32 = DynamicLibrary.open('OLEAUT32.dll');
 
-int SysAddRefString(Pointer<Utf16> bstrString) {
-  final _SysAddRefString = _oleaut32.lookupFunction<
-      Int32 Function(Pointer<Utf16> bstrString),
-      int Function(Pointer<Utf16> bstrString)>('SysAddRefString');
-  return _SysAddRefString(bstrString);
-}
+int SysAddRefString(Pointer<Utf16> bstrString) => _SysAddRefString(bstrString);
 
-Pointer<Utf16> SysAllocString(Pointer<Utf16> psz) {
-  final _SysAllocString = _oleaut32.lookupFunction<
-      Pointer<Utf16> Function(Pointer<Utf16> psz),
-      Pointer<Utf16> Function(Pointer<Utf16> psz)>('SysAllocString');
-  return _SysAllocString(psz);
-}
+late final _SysAddRefString = _oleaut32.lookupFunction<
+    Int32 Function(Pointer<Utf16> bstrString),
+    int Function(Pointer<Utf16> bstrString)>('SysAddRefString');
 
-Pointer<Utf16> SysAllocStringByteLen(Pointer<Utf8> psz, int len) {
-  final _SysAllocStringByteLen = _oleaut32.lookupFunction<
-      Pointer<Utf16> Function(Pointer<Utf8> psz, Uint32 len),
-      Pointer<Utf16> Function(
-          Pointer<Utf8> psz, int len)>('SysAllocStringByteLen');
-  return _SysAllocStringByteLen(psz, len);
-}
+Pointer<Utf16> SysAllocString(Pointer<Utf16> psz) => _SysAllocString(psz);
 
-Pointer<Utf16> SysAllocStringLen(Pointer<Utf16> strIn, int ui) {
-  final _SysAllocStringLen = _oleaut32.lookupFunction<
-      Pointer<Utf16> Function(Pointer<Utf16> strIn, Uint32 ui),
-      Pointer<Utf16> Function(
-          Pointer<Utf16> strIn, int ui)>('SysAllocStringLen');
-  return _SysAllocStringLen(strIn, ui);
-}
+late final _SysAllocString = _oleaut32.lookupFunction<
+    Pointer<Utf16> Function(Pointer<Utf16> psz),
+    Pointer<Utf16> Function(Pointer<Utf16> psz)>('SysAllocString');
 
-void SysFreeString(Pointer<Utf16> bstrString) {
-  final _SysFreeString = _oleaut32.lookupFunction<
-      Void Function(Pointer<Utf16> bstrString),
-      void Function(Pointer<Utf16> bstrString)>('SysFreeString');
-  return _SysFreeString(bstrString);
-}
+Pointer<Utf16> SysAllocStringByteLen(Pointer<Utf8> psz, int len) =>
+    _SysAllocStringByteLen(psz, len);
 
-int SysReAllocString(Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz) {
-  final _SysReAllocString = _oleaut32.lookupFunction<
-      Int32 Function(Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz),
-      int Function(Pointer<Pointer<Utf16>> pbstr,
-          Pointer<Utf16> psz)>('SysReAllocString');
-  return _SysReAllocString(pbstr, psz);
-}
+late final _SysAllocStringByteLen = _oleaut32.lookupFunction<
+    Pointer<Utf16> Function(Pointer<Utf8> psz, Uint32 len),
+    Pointer<Utf16> Function(
+        Pointer<Utf8> psz, int len)>('SysAllocStringByteLen');
+
+Pointer<Utf16> SysAllocStringLen(Pointer<Utf16> strIn, int ui) =>
+    _SysAllocStringLen(strIn, ui);
+
+late final _SysAllocStringLen = _oleaut32.lookupFunction<
+    Pointer<Utf16> Function(Pointer<Utf16> strIn, Uint32 ui),
+    Pointer<Utf16> Function(Pointer<Utf16> strIn, int ui)>('SysAllocStringLen');
+
+void SysFreeString(Pointer<Utf16> bstrString) => _SysFreeString(bstrString);
+
+late final _SysFreeString = _oleaut32.lookupFunction<
+    Void Function(Pointer<Utf16> bstrString),
+    void Function(Pointer<Utf16> bstrString)>('SysFreeString');
+
+int SysReAllocString(Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz) =>
+    _SysReAllocString(pbstr, psz);
+
+late final _SysReAllocString = _oleaut32.lookupFunction<
+    Int32 Function(Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz),
+    int Function(
+        Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz)>('SysReAllocString');
 
 int SysReAllocStringLen(
-    Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz, int len) {
-  final _SysReAllocStringLen = _oleaut32.lookupFunction<
-      Int32 Function(
-          Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz, Uint32 len),
-      int Function(Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz,
-          int len)>('SysReAllocStringLen');
-  return _SysReAllocStringLen(pbstr, psz, len);
-}
+        Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz, int len) =>
+    _SysReAllocStringLen(pbstr, psz, len);
 
-void SysReleaseString(Pointer<Utf16> bstrString) {
-  final _SysReleaseString = _oleaut32.lookupFunction<
-      Void Function(Pointer<Utf16> bstrString),
-      void Function(Pointer<Utf16> bstrString)>('SysReleaseString');
-  return _SysReleaseString(bstrString);
-}
+late final _SysReAllocStringLen = _oleaut32.lookupFunction<
+    Int32 Function(
+        Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz, Uint32 len),
+    int Function(Pointer<Pointer<Utf16>> pbstr, Pointer<Utf16> psz,
+        int len)>('SysReAllocStringLen');
 
-int SysStringByteLen(Pointer<Utf16> bstr) {
-  final _SysStringByteLen = _oleaut32.lookupFunction<
-      Uint32 Function(Pointer<Utf16> bstr),
-      int Function(Pointer<Utf16> bstr)>('SysStringByteLen');
-  return _SysStringByteLen(bstr);
-}
+void SysReleaseString(Pointer<Utf16> bstrString) =>
+    _SysReleaseString(bstrString);
 
-int SysStringLen(Pointer<Utf16> pbstr) {
-  final _SysStringLen = _oleaut32.lookupFunction<
-      Uint32 Function(Pointer<Utf16> pbstr),
-      int Function(Pointer<Utf16> pbstr)>('SysStringLen');
-  return _SysStringLen(pbstr);
-}
+late final _SysReleaseString = _oleaut32.lookupFunction<
+    Void Function(Pointer<Utf16> bstrString),
+    void Function(Pointer<Utf16> bstrString)>('SysReleaseString');
+
+int SysStringByteLen(Pointer<Utf16> bstr) => _SysStringByteLen(bstr);
+
+late final _SysStringByteLen = _oleaut32.lookupFunction<
+    Uint32 Function(Pointer<Utf16> bstr),
+    int Function(Pointer<Utf16> bstr)>('SysStringByteLen');
+
+int SysStringLen(Pointer<Utf16> pbstr) => _SysStringLen(pbstr);
+
+late final _SysStringLen = _oleaut32.lookupFunction<
+    Uint32 Function(Pointer<Utf16> pbstr),
+    int Function(Pointer<Utf16> pbstr)>('SysStringLen');
