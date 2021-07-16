f47cf0e15df6f16570cef4b836149b2f87509c0f
Tim Sneath <tim@sneath.org>
2021-07-16 09:57:39 -0700

    Fix sound example

--- example/play_sound.dart
+++ example/play_sound.dart
@@ -3,25 +3,30 @@
 // BSD-style license that can be found in the LICENSE file.
 
 // Play a sound
-// Example comes from:
-//   https://docs.microsoft.com/en-us/windows/win32/gdi/capturing-an-image
 
 import 'dart:io';
+
+import 'package:ffi/ffi.dart';
 
 import 'package:win32/win32.dart';
 
 void main() {
   const logonSound = r'C:\Windows\Media\Windows Logon.wav';
+  final pLogonSound = logonSound.toNativeUtf16();
 
-  final file = File(logonSound).existsSync();
-  if (!file) {
-    print('WAV file missing.');
-    exit(1);
-  } else {
-    final result = PlaySound(TEXT(logonSound), NULL, SND_FILENAME | SND_SYNC);
+  try {
+    final file = File(logonSound).existsSync();
+    if (!file) {
+      print('WAV file missing.');
+      exit(1);
+    } else {
+      final result = PlaySound(pLogonSound, NULL, SND_FILENAME | SND_SYNC);
 
-    if (result != TRUE) {
-      print('Sound playback failed.');
+      if (result != TRUE) {
+        print('Sound playback failed.');
+      }
     }
+  } finally {
+    free(pLogonSound);
   }
 }
