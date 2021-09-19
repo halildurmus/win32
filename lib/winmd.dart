b73a7c1c90dd6f956bbae6e19c401338a1b00267
Tim Sneath <tim@sneath.org>
2021-09-19 11:53:47 -0700

    Add more documentation

--- lib/winmd.dart
+++ lib/winmd.dart
@@ -1,8 +1,12 @@
-/// Support for working with Windows Metadata, as defined in the
-/// [ECMA-335](https://www.ecma-international.org/publications/files/ECMA-ST/ECMA-335.pdf)
-/// specification.
+/// Dart language support for working with Windows Metadata.
 ///
-/// The ECMA-335 format originated as part of the Common Language Infrastructure
+/// This library may be used to interrogate the Windows developer API, whether
+/// unmanaged APIs like Win32 or COM, or APIs like Windows Runtime that include
+/// their own metadata.
+///
+/// The library consumes metadata in the
+/// [ECMA-335](https://www.ecma-international.org/publications/files/ECMA-ST/ECMA-335.pdf)
+/// format, which originated as part of the Common Language Infrastructure
 /// (CLI), part of the standardized interface offered by .NET libraries. Since
 /// its creation, Windows Metadata has been used to provide a [language-neutral
 /// specification of the Windows Runtime
@@ -22,8 +26,26 @@
 /// In addition to the package above and more specialized alternatives, other
 /// tooling may also find this package useful for creating Windows utilities
 /// with Dart or Flutter.
-
-// ignore_for_file: directives_ordering
+///
+/// To start using this library, it's necessary to obtain a [Scope], which
+/// contains a named set of metadata. For most scenarios, you should retrieve a
+/// scope through one of the static methods of the [MetadataStore] class, which
+/// caches retrieved scopes. The `winmd` package embeds the Win32 API metadata
+/// for ease of distribution. so the following can be used to get to the Win32
+/// scope:
+///
+/// ```dart
+///   final scope = MetadataStore.getWin32Scope();
+/// ```
+///
+/// From here, you can interrogate the `scope` object for its children; in
+/// particular, the collection of [TypeDef] objects that in turn contain
+/// [Method], [Field], [Event] and [Property] members.
+///
+/// In general, the model presented by this package is consistent with the APIs
+/// exposed by the
+/// [IMetaDataImport](https://docs.microsoft.com/en-us/windows/win32/api/rometadataapi/nn-rometadataapi-imetadataimport)
+/// COM interface exposed by `rometadata.dll`.
 
 library winmd;
 
