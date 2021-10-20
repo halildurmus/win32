d9d71afc22e474eea298d745ffdd271bd9f72839
Tim Sneath <tim@sneath.org>
2021-10-19 21:10:25 -0700

    Update Flutter sample to support rounded windows

--- example/explorer/lib/main.dart
+++ example/explorer/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:path_provider/path_provider.dart';
 
-import 'volumes.dart';
+import 'data/winver.dart';
+import 'volumepanel.dart';
+import 'windowroundingselector.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,113 +11,63 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        theme: ThemeData(
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        themeMode: ThemeMode.light,
-        home: Scaffold(
-          // This exists only to test that path_provider doesn't break with the
-          // latest Win32. It's entirely redundant to the point of this demo.
-          floatingActionButton: FloatingActionButton.extended(
-              onPressed: () async {
-                final appDocDir = await getApplicationDocumentsDirectory();
-                print(appDocDir.path);
-              },
-              label: Text('Paths')),
-          body: VolumeListView(),
-        ),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      themeMode: ThemeMode.light,
+      home: MainPage(),
+
+      // body: VolumePanel(),
+      // Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+      //     // if (isWindows11) WindowRoundingSelector(),
+      //     // const Center(child: Text('Some text here')),
+      //     VolumePanel(),
+      //   ],
+      // ),
+      // ),
+    );
+  }
 }
 
-class VolumeListView extends StatefulWidget {
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
+
   @override
-  _VolumeListViewState createState() => _VolumeListViewState();
+  _MainPageState createState() => _MainPageState();
 }
 
-class _VolumeListViewState extends State<VolumeListView> {
-  final List<Volume> _volumes = [];
+class _MainPageState extends State<MainPage> {
+  bool showRoundedCornerSwitch = false;
 
   @override
   void initState() {
-    super.initState();
+    showRoundedCornerSwitch = isWindows11();
 
-    _volumes.addAll(Volumes().getVolumes());
+    super.initState();
   }
 
   @override
-  Widget build(BuildContext context) => Container(
-        margin: const EdgeInsets.all(5),
-        child: ListView.builder(
-          itemCount: _volumes.length,
-          itemBuilder: (context, position) => VolumeCard(
-            volume: _volumes[position],
-          ),
-        ),
-      );
-}
-
-class VolumeCard extends StatefulWidget {
-  const VolumeCard({required this.volume});
-
-  final Volume volume;
-
-  @override
-  _VolumeCardState createState() => _VolumeCardState();
-}
-
-class _VolumeCardState extends State<VolumeCard> {
-  @override
-  Widget build(BuildContext context) => Card(
-        child: Padding(
-          padding: const EdgeInsets.all(5),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const FaIcon(FontAwesomeIcons.hdd,
-                        size: 32, color: Colors.blueGrey),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(20, 5, 5, 5),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            widget.volume.deviceName,
-                            style: Theme.of(context).textTheme.subtitle1,
-                          ),
-                          Text(
-                            widget.volume.volumeName,
-                            style: Theme.of(context)
-                                .textTheme
-                                .bodyText2!
-                                .copyWith(
-                                    color: Theme.of(context)
-                                        .textTheme
-                                        .caption
-                                        ?.color),
-                          ),
-                          const Divider(height: 5),
-                          Column(
-                            children: [
-                              for (var path in widget.volume.paths)
-                                Text(
-                                  path,
-                                  style: Theme.of(context).textTheme.bodyText2,
-                                ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      );
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // This exists only to test that path_provider doesn't break with the
+      // latest Win32. It's entirely redundant to the point of this demo.
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () async {
+          final appDocDir = await getApplicationDocumentsDirectory();
+          print(appDocDir.path);
+        },
+        label: Text('Paths'),
+      ),
+      body: Column(
+        children: [
+          if (showRoundedCornerSwitch) WindowRoundingSelector(),
+          Expanded(child: VolumePanel()),
+        ],
+      ),
+    );
+  }
 }
