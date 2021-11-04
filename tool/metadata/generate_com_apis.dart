4db715600ccd87a984b701a11a5a80b7b1446d53
Tim Sneath <tim@sneath.org>
2021-11-04 16:06:54 -0700

    Generate COM classes where applicable

--- tool/metadata/generate_com_apis.dart
+++ tool/metadata/generate_com_apis.dart
@@ -5,6 +5,7 @@ import 'dart:io';
 import 'package:args/args.dart';
 import 'package:winmd/winmd.dart';
 
+import '../projection/class.dart';
 import '../projection/interface.dart';
 import '../projection/utils.dart';
 
@@ -101,19 +102,23 @@ void main(List<String> args) {
   final classDirectory = Directory(argResults['classDirectory'] as String);
   // final testDirectory = Directory(argResults['testDirectory'] as String);
 
-  for (final type in interfacesToGenerate) {
-    final mdTypeDef = scope.findTypeDef(type);
-
-    if (mdTypeDef == null) throw Exception("Can't find $type");
+  for (final interface in interfacesToGenerate) {
+    final typeDef = scope.findTypeDef(interface);
+    if (typeDef == null) throw Exception("Can't find $interface");
 
     InterfaceProjection interfaceProjection;
-    interfaceProjection = InterfaceProjection(mdTypeDef);
+    interfaceProjection = InterfaceProjection(typeDef);
 
-    // TODO: Figure out when to class projections
+    // In v2, we put classes and interfaces in the same file.
+    final className = ClassProjection.generateClassName(typeDef);
+    if (scope.findTypeDef(className) != null) {
+      interfaceProjection = ClassProjection.fromInterface(typeDef);
+    }
 
     final dartClass = interfaceProjection.toString();
 
-    final classOutputFilename = stripAnsiUnicodeSuffix(type.split('.').last);
+    final classOutputFilename =
+        stripAnsiUnicodeSuffix(interface.split('.').last);
     final outputFile =
         File('${classDirectory.uri.toFilePath()}$classOutputFilename.dart');
 
