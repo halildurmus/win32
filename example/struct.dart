83c73b1a79b20d6911ee4c1d8b29bdf960298f64
Tim Sneath <tim@sneath.org>
2021-11-17 18:56:22 -0800

    Remove `hive` dependency

--- example/struct.dart
+++ example/struct.dart
@@ -1,28 +1,24 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 
-import 'package:ffi/ffi.dart';
-import 'package:win32/win32.dart';
+// import 'package:ffi/ffi.dart';
+// import 'package:win32/win32.dart';
 import 'package:winmd/winmd.dart';
 
 void main() {
   final scope = MetadataStore.getWin32Scope();
 
   // Find a namesapce
-  final struct = scope
-      .findTypeDef('Windows.Win32.System.Com.StructuredStorage.PROPVARIANT')!;
-  final field = struct.fields.first;
-  final fieldType = field.typeIdentifier.name;
-  print(fieldType);
-  final szTypeDef = fieldType.toNativeUtf16();
+  final struct =
+      scope.findTypeDef('Windows.Win32.UI.Input.KeyboardAndMouse.INPUT')!;
+  final unionField = struct.fields.last; // union
 
-  final ptkTypeDef = calloc<mdTypeDef>();
-  final hr =
-      scope.reader.FindTypeDefByName(szTypeDef, field.parent.token, ptkTypeDef);
-  if (SUCCEEDED(hr)) {
-    print(ptkTypeDef.value.toHexString(32));
+  final nestedUnion = unionField.nestedType!;
+  print(nestedUnion);
+  print('contains>');
+  for (final field in nestedUnion.fields) {
+    print(' - ${field.typeIdentifier.name} $field');
   }
 
-  final nestedType = scope.findTypeDefByToken(ptkTypeDef.value);
-
-  print('found: $nestedType');
+  print(nestedUnion.isUnion);
+  print(nestedUnion.parent?.name);
 }
