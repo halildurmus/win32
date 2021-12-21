48e6cc880405e0d9db99481f72e4a87ee921f66b
Tim Sneath <timsneath@google.com>
2021-12-21 14:52:01 -0800

    Generate a library for each feature area

--- tool/v3/library.dart
+++ tool/v3/library.dart
@@ -2,11 +2,11 @@ import 'dart:io';
 
 import 'generate.dart';
 
-const win32FileHeader = '''
+const libraryHeader = '''
 // ignore_for_file: directives_ordering
+''';
 
-library new_win32;
-
+const libraryDefaultExports = '''
 export 'src/constants_new.dart';
 export 'src/exceptions.dart';
 export 'src/macros.dart';
@@ -22,21 +22,48 @@ export 'src/extensions/set_string_array.dart';
 export 'src/extensions/unpack_utf16.dart';
 ''';
 
+/// A mapping of feature areas to export statements
+final exports = <String, List<String>>{};
+
+/// For a given Win32 namespace, return its top-level feature area.
+///
+/// For example, `Windows.Win32.Media.Audio.DirectMusic` resolves to `Media`.
+String featureArea(String namespace) => namespace.split('.')[2];
+
+/// Add a new export to the exports map.
+void addExport(String featureArea, String export) {
+  if (!exports.containsKey(featureArea)) {
+    exports[featureArea] = [export];
+  } else {
+    exports[featureArea]!.add(export);
+  }
+}
+
+/// For each namespace, add all generated .dart files in the directory
 void generateLibraryExport(List<String> namespaces) {
-  final buffer = StringBuffer();
-  buffer.writeln(win32FileHeader);
   for (final namespace in namespaces) {
+    final area = featureArea(namespace);
     final directory = Directory(folderForNamespace(namespace));
 
     final relativePath = folderForNamespace(namespace).substring(4);
     for (final file in directory.listSync()) {
       if (file.existsSync() && file.uri.toFilePath().endsWith('.dart')) {
         final export = "export '$relativePath/${file.path.split('\\').last}';";
-        buffer.writeln(export);
+        addExport(area, export);
       }
     }
   }
 
-  final libraryFile = formatter.format(buffer.toString());
-  File('lib/new_win32.dart').writeAsStringSync(libraryFile);
+  /// Write a separate export for each feature area
+  for (final featureArea in exports.keys) {
+    final exportFile = [
+      libraryHeader,
+      'library $featureArea;',
+      libraryDefaultExports,
+      ...exports[featureArea]!
+    ].join('\n');
+
+    File('lib/$featureArea.dart')
+        .writeAsStringSync(formatter.format(exportFile));
+  }
 }
