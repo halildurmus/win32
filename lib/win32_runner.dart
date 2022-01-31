5712204d7828ed56c04c19e8fe11054a23ac1086
Tim Sneath <tim@sneath.org>
2022-01-30 16:25:33 -0800

    Rename embedder file

--- lib/win32_runner.dart
+++ lib/win32_runner.dart
@@ -1,7 +1,7 @@
 library win32_runner;
 
 export 'src/dart_project.dart';
-export 'src/engine.dart';
+export 'src/embedder.dart';
 export 'src/ffi.dart';
 export 'src/utils.dart';
 export 'src/window.dart';
