0a29bcd65bb0f852459a35377d07178b2c85882d
Tim Sneath <tim@sneath.org>
2022-02-17 11:33:42 -0800

    Add Flutter example

---
+++ example/inspector/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+void main() {
+  runApp(const GamepadApp());
+}
+
+class GamepadApp extends StatelessWidget {
+  const GamepadApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
