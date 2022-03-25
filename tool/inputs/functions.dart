43fa7ec3b184ac4e43e1985fc0e13bb36c35acc4
Sunbreak <sunbreak.wang@gmail.com>
2022-03-25 23:05:43 +0800

    Add setupapi.dart
    Signed-off-by: Sunbreak <sunbreak.wang@gmail.com>

--- tool/inputs/functions.dart
+++ tool/inputs/functions.dart
@@ -13,7 +13,7 @@ const dllLibraries = [
   'advapi32', 'bthprops', 'bluetoothapis', 'comctl32', 'comdlg32', 'dbghelp',
   'dwmapi', 'dxva2',
   'gdi32', 'kernel32', 'kernelbase', 'magnification', 'ole32', 'oleaut32',
-  'powrprof', 'rometadata', 'scarddlg', 'shcore', 'shell32', 'spoolss',
+  'powrprof', 'rometadata', 'scarddlg', 'setupapi', 'shcore', 'shell32', 'spoolss',
   'user32', 'uxtheme', 'version', 'xinput1_4', 'winmm', 'winscard',
   'winspool', 'wlanapi', 'ws2_32'
 ];
