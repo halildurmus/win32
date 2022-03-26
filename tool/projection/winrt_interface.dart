22c6b8bc9f18e3be5fdcc752ccdf1e6bbbbe45a9
Tim Sneath <tim@sneath.org>
2022-03-26 16:21:26 -0700

    Tidy up old code

--- tool/projection/winrt_interface.dart
+++ tool/projection/winrt_interface.dart
@@ -65,40 +65,6 @@ class WinRTInterfaceProjection extends InterfaceProjection {
     return projection;
   }
 
-  /// Take a TypeDef and create a Dart projection of it.
-  void projection() {
-    // TODO: Refactor this into smaller pieces
-    // var parent = '';
-
-    // WinRT interfaces don't inherit in metadata (e.g. IAsyncInfo has no
-    // parent), but all WinRT interfaces have a base type of IInspectable as far
-    // as the COM vtable is concerned. They contain the functions of
-    // IInspectable (as well as IUnknown, from which IInspectable itself
-    // inherits).
-
-    // final classInheritsFrom = parent;
-
-    // // final interface = ClassProjection(
-    // //     sourceType: typeDef.name.startsWith('Windows.Win32')
-    // //         ? SourceType.com
-    // //         : SourceType.winrt,
-    // //     iid: typeDef.guid,
-    // //     name: stripAnsiUnicodeSuffix(typeDef.name),
-    // //     inherits: classInheritsFrom,
-    // //     vtableStart: _vtableStart(typeDef));
-
-    // if (typeDef.genericParams.isNotEmpty) {
-    //   final genericParams =
-    //       typeDef.genericParams.map<String>((p) => '${p.name}, ').join();
-    //   interface.shortNameWithGenericSpecifier =
-    //       '${interface.shortName}<$genericParams>';
-    // } else {
-    //   interface.shortNameWithGenericSpecifier = interface.shortName;
-    // }
-
-    // return interface;
-  }
-
   @override
   int cacheVtableStart(TypeDef? type) {
     if (type == null) {
@@ -120,6 +86,11 @@ class WinRTInterfaceProjection extends InterfaceProjection {
     return 0;
   }
 
+  // WinRT interfaces don't inherit in metadata (e.g. IAsyncInfo has no
+  // parent), but all WinRT interfaces have a base type of IInspectable as far
+  // as the COM vtable is concerned. They contain the functions of
+  // IInspectable (as well as IUnknown, from which IInspectable itself
+  // inherits).
   @override
   String get rootHeader => "import 'IInspectable.dart';";
 
@@ -131,26 +102,4 @@ class WinRTInterfaceProjection extends InterfaceProjection {
 
   @override
   String get shortName => typeDef.name.split('.').last.split('`').first;
-
-  void stuff() {
-    // WinRT methods always return an HRESULT, and provide the actual return
-    //   // value as an pointer
-    //   methodProjection.returnTypeNative = 'Int32';
-    //   methodProjection.returnTypeDart = 'int';
-    //   if (method.returnType.typeIdentifier.baseType != winmd.BaseType.Void) {
-    //     final typeBuilder = TypeProjection(method.returnType.typeIdentifier);
-
-    //     if (method.isSetProperty) {
-    //       final paramName = methodProjection.name.substring(4).toCamelCase();
-    //       methodProjection.parameters
-    //           .add(ParameterProjection(paramName, typeBuilder));
-    //     } else if (method.isGetProperty) {
-    //       methodProjection.parameters
-    //           .add(ParameterProjection('value', typeBuilder));
-    //     } else {
-    //       // TODO: needs wrapping in a Pointer<> when it's printed
-    //       methodProjection.parameters
-    //           .add(ParameterProjection('result', typeBuilder));
-    //     }
-  }
 }
