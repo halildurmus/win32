f833b98461fd1bdd02bb038488ba4a24323625fd
Tim Sneath <tim@sneath.org>
2022-03-26 18:58:52 -0700

    Add additional generated classes

--- lib/src/com/IStringable.dart
+++ lib/src/com/IStringable.dart
@@ -2,41 +2,70 @@
 
 // THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
 
-// ignore_for_file: unused_import
+// ignore_for_file: unused_import, directives_ordering
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+// ignore_for_file: no_leading_underscores_for_local_identifiers
 
 import 'dart:ffi';
 
 import 'package:ffi/ffi.dart';
 
+import '../callbacks.dart';
 import '../combase.dart';
 import '../constants.dart';
 import '../exceptions.dart';
+import '../guid.dart';
 import '../macros.dart';
 import '../ole32.dart';
 import '../structs.dart';
 import '../structs.g.dart';
 import '../utils.dart';
 
-import '../winrt/winrt_constants.dart';
+import '../api-ms-win-core-winrt-string-l1-1-0.dart';
+import '../winrt/winrt_helpers.dart';
+import '../types.dart';
 
 import 'IInspectable.dart';
 
 /// @nodoc
 const IID_IStringable = '{96369F54-8EB6-48F0-ABCE-C1B211E627C3}';
 
-typedef _ToString_Native = Int32 Function(Pointer obj, Pointer<IntPtr> result);
-typedef _ToString_Dart = int Function(Pointer obj, Pointer<IntPtr> result);
-
 /// {@category Interface}
 /// {@category winrt}
 class IStringable extends IInspectable {
-  // vtable begins at 6, ends at 6
-
+  // vtable begins at 6, is 1 entries long.
   IStringable(Pointer<COMObject> ptr) : super(ptr);
 
-  int ToString(Pointer<IntPtr> result) => ptr.ref.lpVtbl.value
-      .elementAt(6)
-      .cast<Pointer<NativeFunction<_ToString_Native>>>()
-      .value
-      .asFunction<_ToString_Dart>()(ptr.ref.lpVtbl, result);
+  String ToString() {
+    final retValuePtr = calloc<HSTRING>();
+
+    try {
+      final hr = ptr.ref.lpVtbl.value
+          .elementAt(6)
+          .cast<
+              Pointer<
+                  NativeFunction<
+                      HRESULT Function(
+            Pointer,
+            Pointer<IntPtr>,
+          )>>>()
+          .value
+          .asFunction<
+              int Function(
+            Pointer,
+            Pointer<IntPtr>,
+          )>()(
+        ptr.ref.lpVtbl,
+        retValuePtr,
+      );
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      final retValue = retValuePtr.toDartString();
+      return retValue;
+    } finally {
+      WindowsDeleteString(retValuePtr.value);
+      free(retValuePtr);
+    }
+  }
 }
