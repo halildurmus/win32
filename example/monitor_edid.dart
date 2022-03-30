57c83a53326c3f56bd0b9bb6a7a1ff62cb47ea03
Tim Sneath <timsneath@google.com>
2022-03-29 19:45:33 -0700

    Remove GUID (it's in constants.dart now)

--- example/monitor_edid.dart
+++ example/monitor_edid.dart
@@ -17,7 +17,6 @@ class Size {
 }
 
 Size getMonitorSizeInMM() {
-  const GUID_CLASS_MONITOR = '{4d36e96e-e325-11ce-bfc1-08002be10318}';
   final guidptr = calloc<GUID>();
 
   guidptr.ref.setGUID(GUID_CLASS_MONITOR);
