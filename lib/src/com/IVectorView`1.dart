3c42d65a1ac9fbf33d52142d3dcf9d0cf27330fb
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-04-15 11:33:31 +0300

    Project `IVectorView` manually

---
+++ lib/src/com/IVectorView`1.dart
@@ -0,0 +1,165 @@
+// IVectorView.dart
+
+// ignore_for_file: unused_import, directives_ordering
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+
+import '../api-ms-win-core-winrt-string-l1-1-0.dart';
+import '../callbacks.dart';
+import '../combase.dart';
+import '../constants.dart';
+import '../exceptions.dart';
+import '../guid.dart';
+import '../macros.dart';
+import '../ole32.dart';
+import '../structs.dart';
+import '../structs.g.dart';
+import '../types.dart';
+import '../utils.dart';
+import '../winrt/winrt_helpers.dart';
+import 'IInspectable.dart';
+
+/// @nodoc
+const IID_IVectorView = '{BBE1FA4C-B0E3-4583-BAEF-1F1B2E483E56}';
+
+/// {@category Interface}
+/// {@category winrt}
+class IVectorView extends IInspectable {
+  // vtable begins at 6, is 4 entries long.
+  IVectorView(Pointer<COMObject> ptr) : super(ptr);
+
+  String GetAt(int index) {
+    final retValuePtr = calloc<HSTRING>();
+
+    try {
+      final hr = ptr.ref.vtable
+          .elementAt(6)
+          .cast<
+              Pointer<
+                  NativeFunction<
+                      HRESULT Function(
+            Pointer,
+            Uint32,
+            Pointer<HSTRING>,
+          )>>>()
+          .value
+          .asFunction<
+              int Function(
+            Pointer,
+            int,
+            Pointer<HSTRING>,
+          )>()(ptr.ref.lpVtbl, index, retValuePtr);
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      final retValue = retValuePtr.toDartString();
+      return retValue;
+    } finally {
+      WindowsDeleteString(retValuePtr.value);
+      free(retValuePtr);
+    }
+  }
+
+  int get Size {
+    final retValuePtr = calloc<Uint32>();
+
+    try {
+      final hr = ptr.ref.vtable
+          .elementAt(7)
+          .cast<
+              Pointer<
+                  NativeFunction<
+                      HRESULT Function(
+            Pointer,
+            Pointer<Uint32>,
+          )>>>()
+          .value
+          .asFunction<
+              int Function(
+            Pointer,
+            Pointer<Uint32>,
+          )>()(ptr.ref.lpVtbl, retValuePtr);
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      final retValue = retValuePtr.value;
+      return retValue;
+    } finally {
+      free(retValuePtr);
+    }
+  }
+
+  bool IndexOf(String value, Pointer<Uint32> index) {
+    final retValuePtr = calloc<Bool>();
+    final hElement = convertToHString(value);
+
+    try {
+      final hr = ptr.ref.vtable
+          .elementAt(8)
+          .cast<
+              Pointer<
+                  NativeFunction<
+                      HRESULT Function(
+            Pointer,
+            HSTRING,
+            Pointer<Uint32>,
+            Pointer<Bool>,
+          )>>>()
+          .value
+          .asFunction<
+              int Function(
+            Pointer,
+            int,
+            Pointer<Uint32>,
+            Pointer<Bool>,
+          )>()(ptr.ref.lpVtbl, hElement, index, retValuePtr);
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      final retValue = retValuePtr.value;
+      return retValue;
+    } finally {
+      free(retValuePtr);
+      WindowsDeleteString(hElement);
+    }
+  }
+
+  int GetMany(int startIndex, Pointer<HSTRING> items) {
+    final retValuePtr = calloc<Uint32>();
+
+    try {
+      final hr = ptr.ref.vtable
+          .elementAt(9)
+          .cast<
+              Pointer<
+                  NativeFunction<
+                      HRESULT Function(
+            Pointer,
+            Uint32,
+            Uint32,
+            Pointer<HSTRING>,
+            Pointer<Uint32>,
+          )>>>()
+          .value
+          .asFunction<
+              int Function(
+            Pointer,
+            int,
+            int,
+            Pointer<HSTRING>,
+            Pointer<Uint32>,
+          )>()(ptr.ref.lpVtbl, startIndex, Size, items, retValuePtr);
+
+      if (FAILED(hr)) throw WindowsException(hr);
+
+      final retValue = retValuePtr.value;
+      return retValue;
+    } finally {
+      free(retValuePtr);
+    }
+  }
+}
