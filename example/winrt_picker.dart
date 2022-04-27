c9b0bdb954246a6316325bbeb5e068b910ef6551
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-04-27 13:36:36 +0300

    Update example/winrt_picker.dart

--- example/winrt_picker.dart
+++ example/winrt_picker.dart
@@ -9,21 +9,10 @@ import 'dart:ffi';
 import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
-List<String> hStringArrayToList(Pointer<HSTRING> pHStringArray, int size) {
-  final list = <String>[];
-  for (var i = 0; i < size; i++) {
-    final element = pHStringArray[i];
-    if (element != 0) {
-      list.add(convertFromHString(pHStringArray[i]));
-      WindowsDeleteString(pHStringArray[i]);
-    }
-  }
-
-  return list;
-}
-
 void main() async {
   winrtInitialize();
+
+  final allocator = Arena();
 
   final object = CreateObject(
       'Windows.Storage.Pickers.FileOpenPicker', IID_IFileOpenPicker);
@@ -32,71 +21,67 @@ void main() async {
     ..SuggestedStartLocation = PickerLocationId.Desktop
     ..ViewMode = PickerViewMode.Thumbnail;
 
-  final vector = IVector(picker.FileTypeFilter);
+  // final pIID = convertToIID(IID_IInitializeWithWindow);
+  // final pObject = calloc<COMObject>();
+  // var hr = picker.QueryInterface(pIID, pObject.cast());
+  // if (FAILED(hr)) throw WindowsException(hr);
+  // final initializeWithWindow = IInitializeWithWindow(pObject);
+  // hr = initializeWithWindow.Initialize(GetForegroundWindow());
+  // if (FAILED(hr)) throw WindowsException(hr);
+  // free(pIID);
 
-  print('Vector has ${vector.Size} elements.');
+  final filters = picker.FileTypeFilter;
+
+  print('Vector has ${filters.Size} elements.');
   print('Adding ".jpg" to the vector...');
-  vector.Append('.jpg');
-  print('Vector has ${vector.Size} elements.');
-  print('Vector\'s first element is ${vector.GetAt(0)}.');
+  filters.Append('.jpg');
+  print('Vector has ${filters.Size} elements.');
+  print('Vector\'s first element is ${filters.GetAt(0)}.');
   print('Adding ".jpeg" to the vector...');
-  vector.Append('.jpeg');
-  print('Vector\'s second element is ${vector.GetAt(1)}.');
+  filters.Append('.jpeg');
+  print('Vector\'s second element is ${filters.GetAt(1)}.');
 
-  final vectorView = vector.GetView;
-  print('VectorView has ${vectorView.Size} elements.');
+  final vectorView = filters.GetView;
+  print('VectorView has ${vectorView.length} elements.');
 
-  final pIndex = calloc<Uint32>();
-  var containsElement = vector.IndexOf('.jpeg', pIndex);
+  final pIndex = allocator<Uint32>();
+  var containsElement = filters.IndexOf('.jpeg', pIndex);
   print(containsElement
       ? 'The index of ".jpeg" is ${pIndex.value}.'
       : 'The ".jpeg" does not exists in the vector!');
-  containsElement = vector.IndexOf('.txt', pIndex);
+
+  containsElement = filters.IndexOf('.txt', pIndex);
   print(containsElement
       ? 'The index of ".txt" is ${pIndex.value}.'
       : 'The ".txt" does not exists in the vector!');
 
   print('Setting vector\'s first element to ".png"...');
-  vector.SetAt(0, '.png');
-  print('Vector\'s first element is ${vector.GetAt(0)}.');
+  filters.SetAt(0, '.png');
+  print('Vector\'s first element is ${filters.GetAt(0)}.');
 
   print('Inserting ".gif" to the vector\'s first index...');
-  vector.InsertAt(0, '.gif');
-  print('Vector has ${vector.Size} elements.');
-  print('Vector\'s first element is ${vector.GetAt(0)}.');
+  filters.InsertAt(0, '.gif');
+  print('Vector has ${filters.Size} elements.');
+  print('Vector\'s first element is ${filters.GetAt(0)}.');
 
   print('Removing the vector\'s last element...');
-  vector.RemoveAtEnd();
-  print('Vector has ${vector.Size} elements.');
-  print('Vector\'s last element is ${vector.GetAt(vector.Size - 1)}.');
+  filters.RemoveAtEnd();
+  print('Vector has ${filters.Size} elements.');
+  print('Vector\'s last element is ${filters.GetAt(filters.Size - 1)}.');
 
-  final pArray = calloc<HSTRING>(sizeOf<HSTRING>() * vector.Size);
-  var items = vector.GetMany(0, pArray);
-  print(items > 0
-      ? 'Vector elements: ${hStringArrayToList(pArray, vector.Size)}'
-      : 'Vector is empty!');
+  var list = filters.GetView;
+  print(list.isNotEmpty ? 'Vector elements: $list' : 'Vector is empty!');
 
   print('Replacing vector\'s elements with [".jpg", ".jpeg", ".png"]...');
-  vector.ReplaceAll(['.jpg', '.jpeg', '.png']);
-  items = vector.GetMany(0, pArray);
-  print(items > 0
-      ? 'Vector elements: ${hStringArrayToList(pArray, vector.Size)}'
-      : 'Vector is empty!');
+  filters.ReplaceAll(['.jpg', '.jpeg', '.png']);
 
-  // Does not work yet
-  // Possibly due to https://docs.microsoft.com/en-us/windows/apps/develop/ui-input/display-ui-objects
-  picker.PickSingleFileAsync();
+  list = filters.GetView;
+  print(list.isNotEmpty ? 'Vector elements: $list' : 'Vector is empty!');
 
   print('Clearing the vector...');
-  vector.Clear();
-  print('Vector has ${vector.Size} elements.');
+  filters.Clear();
+  print('Vector has ${filters.Size} elements.');
 
-  await Future<void>.delayed(const Duration(seconds: 5));
-
-  free(vector.ptr);
-  free(vectorView.ptr);
-  free(pIndex);
-  free(pArray);
+  free(filters.ptr);
   winrtUninitialize();
-  print('Complete');
 }
