fb7f969ec9ae02c0ec399d1553b5f1ed18c4faac
Tim Sneath <timsneath@google.com>
2022-05-08 16:48:33 -0700

    Add method implementation features tests

--- test/method_test.dart
+++ test/method_test.dart
@@ -27,4 +27,35 @@ void main() {
     expect(method.isAbstract, isTrue);
     expect(method.isVirtual, isTrue);
   });
+
+  test('Method properties 3', () {
+    const methodName = 'DoDragDrop';
+
+    final scope = MetadataStore.getWin32Scope();
+    final typeDef = scope.findTypeDef('Windows.Win32.System.Ole.Apis');
+    final method = typeDef!.findMethod(methodName)!;
+
+    expect(method.isHideBySig, isTrue);
+    expect(method.vTableLayout, equals(VtableLayout.reuseSlot));
+    expect(method.isStatic, isTrue);
+    expect(method.isAbstract, isFalse);
+    expect(method.isPinvokeImpl, isTrue);
+    expect(method.pinvokeMap.isNoMangle, isTrue);
+    expect(method.pinvokeMap.importName, equals(methodName));
+
+    final importDllToken = method.pinvokeMap.importDllToken;
+    final moduleRef = ModuleRef.fromToken(scope, importDllToken);
+    expect(moduleRef.name, equals('OLE32'));
+
+    expect(
+        method.pinvokeMap.callingConvention, equals(CallingConvention.winApi));
+
+    expect(method.implFeatures.isAggressiveInlining, isFalse);
+    expect(method.implFeatures.isForwardRef, isFalse);
+    expect(method.implFeatures.isManaged, isTrue);
+    expect(method.implFeatures.isNoInlining, isFalse);
+    expect(method.implFeatures.isNoOptimization, isFalse);
+    expect(method.implFeatures.isPreserveSig, isTrue);
+    expect(method.implFeatures.isSynchronized, isFalse);
+  });
 }
