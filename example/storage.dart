3bb373a7da16a150b727459593a4891b222ce8c5
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-06-03 15:53:44 +0300

    Fix storage example

--- example/storage.dart
+++ example/storage.dart
@@ -1,16 +1,15 @@
 import 'package:win32/win32.dart';
 
 void main() {
-  final hr = RoInitialize(RO_INIT_TYPE.RO_INIT_SINGLETHREADED);
-  if (FAILED(hr)) throw WindowsException(hr);
+  winrtInitialize();
 
   // Requires a package identity.
-  final currAppData = ApplicationData.Current();
+  final currAppData = ApplicationData.Current;
   print(currAppData.trustLevel);
   final localFolder = IStorageItem(currAppData.LocalFolder);
   final localPath = localFolder.Path;
 
   print('Local folder path: $localPath');
 
-  RoUninitialize();
+  winrtUninitialize();
 }
