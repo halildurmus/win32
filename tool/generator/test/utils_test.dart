df7b21578e09c3ee70662b1d1d5a2d7f6ce8869e
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-06-13 17:55:35 +0300

    Add tests for utility functions

--- tool/generator/test/utils_test.dart
+++ tool/generator/test/utils_test.dart
@@ -115,4 +115,122 @@ void main() {
     expect(libraryFromDllName('bthprops'), equals('bthprops.cpl'));
     expect(libraryFromDllName('winspool'), equals('winspool.drv'));
   });
+
+  test('folderFromNamespace', () {
+    expect(folderFromNamespace('Windows.Win32.Globalization.ISpellChecker'),
+        equals('globalization'));
+    expect(
+        folderFromNamespace('Windows.Win32.System.Console.CONSOLE_FONT_INFO'),
+        equals('system/console'));
+    expect(folderFromNamespace('Windows.Win32.UI.Shell.Common.SHITEMID'),
+        equals('ui/shell/common'));
+  });
+
+  test('folderFromWinRTNamespace', () {
+    expect(folderFromWinRTNamespace('Windows.Globalization.Calendar'),
+        equals('globalization'));
+    expect(folderFromWinRTNamespace('Windows.Storage.Pickers.FileOpenPicker'),
+        equals('storage/pickers'));
+    expect(
+        folderFromWinRTNamespace(
+            'Windows.Devices.Geolocation.Geofencing.Geofence'),
+        equals('devices/geolocation/geofencing'));
+  });
+
+  test('filePathFromWinRTNamespace', () {
+    expect(filePathFromWinRTNamespace('Windows.Globalization.Calendar'),
+        equals('globalization/calendar.dart'));
+    expect(filePathFromWinRTNamespace('Windows.Storage.Pickers.FileOpenPicker'),
+        equals('storage/pickers/fileopenpicker.dart'));
+    expect(
+        filePathFromWinRTNamespace(
+            'Windows.Devices.Geolocation.Geofencing.Geofence'),
+        equals('devices/geolocation/geofencing/geofence.dart'));
+  });
+
+  test('parentNamespace', () {
+    expect(parentNamespace('Windows.Foundation.IAsyncInfo'),
+        equals('Windows.Foundation'));
+    expect(parentNamespace('Windows.Gaming.Input.Gamepad'),
+        equals('Windows.Gaming.Input'));
+    expect(parentNamespace('Windows.Devices.Display.Core.DisplayManager'),
+        equals('Windows.Devices.Display.Core'));
+  });
+
+  test('groupTypesByParentNamespace', () {
+    expect(
+        groupTypesByParentNamespace([
+          'Windows.Data.Xml.Dom.IXmlNodeList',
+          'Windows.Foundation.IAsyncInfo',
+          'Windows.Foundation.IClosable',
+          'Windows.Foundation.IPropertyValue',
+          'Windows.Foundation.IStringable',
+          'Windows.Foundation.PropertyValue',
+          'Windows.Gaming.Input.Gamepad',
+          'Windows.Globalization.Calendar',
+          'Windows.Globalization.PhoneNumberFormatting.PhoneNumberFormatter',
+          'Windows.Globalization.PhoneNumberFormatting.PhoneNumberInfo',
+          'Windows.Networking.IHostName',
+          'Windows.Storage.IApplicationDataStatics',
+          'Windows.Storage.IStorageItem',
+          'Windows.Storage.Pickers.IFileOpenPicker',
+          'Windows.Storage.UserDataPaths',
+          'Windows.UI.Notifications.IToastNotificationManagerStatics',
+          'Windows.UI.Notifications.ToastNotification',
+        ]),
+        equals([
+          NamespaceGroup(
+            namespace: 'Windows.Data.Xml.Dom',
+            types: ['Windows.Data.Xml.Dom.IXmlNodeList'],
+          ),
+          NamespaceGroup(
+            namespace: 'Windows.Foundation',
+            types: [
+              'Windows.Foundation.IAsyncInfo',
+              'Windows.Foundation.IClosable',
+              'Windows.Foundation.IPropertyValue',
+              'Windows.Foundation.IStringable',
+              'Windows.Foundation.PropertyValue'
+            ],
+          ),
+          NamespaceGroup(
+            namespace: 'Windows.Gaming.Input',
+            types: ['Windows.Gaming.Input.Gamepad'],
+          ),
+          NamespaceGroup(
+            namespace: 'Windows.Globalization',
+            types: ['Windows.Globalization.Calendar'],
+          ),
+          NamespaceGroup(
+            namespace: 'Windows.Globalization.PhoneNumberFormatting',
+            types: [
+              'Windows.Globalization.PhoneNumberFormatting.PhoneNumberFormatter',
+              'Windows.Globalization.PhoneNumberFormatting.PhoneNumberInfo'
+            ],
+          ),
+          NamespaceGroup(
+            namespace: 'Windows.Networking',
+            types: ['Windows.Networking.IHostName'],
+          ),
+          NamespaceGroup(
+            namespace: 'Windows.Storage',
+            types: [
+              'Windows.Storage.IApplicationDataStatics',
+              'Windows.Storage.IStorageItem',
+              'Windows.Storage.UserDataPaths'
+            ],
+          ),
+          NamespaceGroup(
+            namespace: 'Windows.Storage.Pickers',
+            types: ['Windows.Storage.Pickers.IFileOpenPicker'],
+          ),
+          NamespaceGroup(
+            namespace: 'Windows.UI.Notifications',
+            types: [
+              'Windows.UI.Notifications.IToastNotificationManagerStatics',
+              'Windows.UI.Notifications.ToastNotification'
+            ],
+          )
+        ]));
+  });
 }
