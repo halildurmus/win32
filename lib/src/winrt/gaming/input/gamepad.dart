bc86287332e17d81e94008cbe2258c87b96ecb84
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-06-13 17:54:45 +0300

    Regenerate WinRT APIs

---
+++ lib/src/winrt/gaming/input/gamepad.dart
@@ -0,0 +1,181 @@
+// gamepad.dart
+
+// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
+
+// ignore_for_file: unused_import, directives_ordering
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+
+import '../../../api_ms_win_core_winrt_string_l1_1_0.dart';
+import '../../../combase.dart';
+import '../../../exceptions.dart';
+import '../../../macros.dart';
+import '../../../utils.dart';
+import '../../../types.dart';
+import '../../../winrt_callbacks.dart';
+import '../../../winrt_helpers.dart';
+
+import '../../../extensions/hstring_array.dart';
+
+import '../../../winrt/gaming/input/igamepad.dart';
+import '../../../winrt/gaming/input/igamecontroller.dart';
+import '../../../winrt/gaming/input/igamepad2.dart';
+import '../../../winrt/gaming/input/igamecontrollerbatteryinfo.dart';
+import 'igamepadstatics.dart';
+import 'igamepadstatics2.dart';
+import '../../../winrt/gaming/input/structs.g.dart';
+import '../../../winrt/gaming/input/headset.dart';
+import '../../../winrt/system/userchangedeventargs.dart';
+import '../../../winrt/system/user.dart';
+import '../../../winrt/devices/power/batteryreport.dart';
+import '../../../winrt/foundation/collections/ivectorview.dart';
+import '../../../com/iinspectable.dart';
+
+/// {@category Class}
+/// {@category winrt}
+class Gamepad extends IInspectable
+    implements
+        IGamepad,
+        IGameController,
+        IGamepad2,
+        IGameControllerBatteryInfo {
+  Gamepad.from(super.ptr);
+
+  static const _className = 'Windows.Gaming.Input.Gamepad';
+
+  // IGamepadStatics methods
+  static int add_GamepadAdded(Pointer<NativeFunction<EventHandler>> value) {
+    final activationFactory =
+        CreateActivationFactory(_className, IID_IGamepadStatics);
+
+    try {
+      return IGamepadStatics.from(activationFactory).add_GamepadAdded(value);
+    } finally {
+      free(activationFactory);
+    }
+  }
+
+  static void remove_GamepadAdded(int token) {
+    final activationFactory =
+        CreateActivationFactory(_className, IID_IGamepadStatics);
+
+    try {
+      return IGamepadStatics.from(activationFactory).remove_GamepadAdded(token);
+    } finally {
+      free(activationFactory);
+    }
+  }
+
+  static int add_GamepadRemoved(Pointer<NativeFunction<EventHandler>> value) {
+    final activationFactory =
+        CreateActivationFactory(_className, IID_IGamepadStatics);
+
+    try {
+      return IGamepadStatics.from(activationFactory).add_GamepadRemoved(value);
+    } finally {
+      free(activationFactory);
+    }
+  }
+
+  static void remove_GamepadRemoved(int token) {
+    final activationFactory =
+        CreateActivationFactory(_className, IID_IGamepadStatics);
+
+    try {
+      return IGamepadStatics.from(activationFactory)
+          .remove_GamepadRemoved(token);
+    } finally {
+      free(activationFactory);
+    }
+  }
+
+  static List<String> get Gamepads {
+    final activationFactory =
+        CreateActivationFactory(_className, IID_IGamepadStatics);
+
+    try {
+      return IGamepadStatics.from(activationFactory).Gamepads;
+    } finally {
+      free(activationFactory);
+    }
+  }
+
+  // IGamepadStatics2 methods
+  static Pointer<COMObject> FromGameController(
+      Pointer<COMObject> gameController) {
+    final activationFactory =
+        CreateActivationFactory(_className, IID_IGamepadStatics2);
+
+    try {
+      return IGamepadStatics2.from(activationFactory)
+          .FromGameController(gameController);
+    } finally {
+      free(activationFactory);
+    }
+  }
+
+  // IGamepad methods
+  late final _iGamepad = IGamepad.from(toInterface(IID_IGamepad));
+
+  @override
+  GamepadVibration get Vibration => _iGamepad.Vibration;
+
+  @override
+  set Vibration(GamepadVibration value) => _iGamepad.Vibration = value;
+
+  @override
+  GamepadReading GetCurrentReading() => _iGamepad.GetCurrentReading();
+  // IGameController methods
+  late final _iGameController =
+      IGameController.from(toInterface(IID_IGameController));
+
+  @override
+  int add_HeadsetConnected(Pointer<NativeFunction<TypedEventHandler>> value) =>
+      _iGameController.add_HeadsetConnected(value);
+
+  @override
+  void remove_HeadsetConnected(int token) =>
+      _iGameController.remove_HeadsetConnected(token);
+
+  @override
+  int add_HeadsetDisconnected(
+          Pointer<NativeFunction<TypedEventHandler>> value) =>
+      _iGameController.add_HeadsetDisconnected(value);
+
+  @override
+  void remove_HeadsetDisconnected(int token) =>
+      _iGameController.remove_HeadsetDisconnected(token);
+
+  @override
+  int add_UserChanged(Pointer<NativeFunction<TypedEventHandler>> value) =>
+      _iGameController.add_UserChanged(value);
+
+  @override
+  void remove_UserChanged(int token) =>
+      _iGameController.remove_UserChanged(token);
+
+  @override
+  Pointer<COMObject> get Headset => _iGameController.Headset;
+
+  @override
+  bool get IsWireless => _iGameController.IsWireless;
+
+  @override
+  Pointer<COMObject> get User => _iGameController.User;
+  // IGamepad2 methods
+  late final _iGamepad2 = IGamepad2.from(toInterface(IID_IGamepad2));
+
+  @override
+  int GetButtonLabel(int button) => _iGamepad2.GetButtonLabel(button);
+  // IGameControllerBatteryInfo methods
+  late final _iGameControllerBatteryInfo = IGameControllerBatteryInfo.from(
+      toInterface(IID_IGameControllerBatteryInfo));
+
+  @override
+  Pointer<COMObject> TryGetBatteryReport() =>
+      _iGameControllerBatteryInfo.TryGetBatteryReport();
+}
---
+++ lib/src/winrt/ui/notifications/toastnotification.dart
@@ -0,0 +1,162 @@
+// toastnotification.dart
+
+// THIS FILE IS GENERATED AUTOMATICALLY AND SHOULD NOT BE EDITED DIRECTLY.
+
+// ignore_for_file: unused_import, directives_ordering
+// ignore_for_file: constant_identifier_names, non_constant_identifier_names
+// ignore_for_file: no_leading_underscores_for_local_identifiers
+
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+
+import '../../../api_ms_win_core_winrt_string_l1_1_0.dart';
+import '../../../combase.dart';
+import '../../../exceptions.dart';
+import '../../../macros.dart';
+import '../../../utils.dart';
+import '../../../types.dart';
+import '../../../winrt_callbacks.dart';
+import '../../../winrt_helpers.dart';
+
+import '../../../extensions/hstring_array.dart';
+
+import '../../../winrt/ui/notifications/itoastnotification.dart';
+import '../../../winrt/ui/notifications/itoastnotification2.dart';
+import '../../../winrt/ui/notifications/itoastnotification3.dart';
+import '../../../winrt/ui/notifications/itoastnotification4.dart';
+import '../../../winrt/ui/notifications/itoastnotification6.dart';
+import 'itoastnotificationfactory.dart';
+import '../../../winrt/data/xml/dom/xmldocument.dart';
+import '../../../winrt/foundation/ireference.dart';
+import '../../../winrt/ui/notifications/toastdismissedeventargs.dart';
+import '../../../winrt/ui/notifications/toastfailedeventargs.dart';
+import '../../../winrt/ui/notifications/notificationdata.dart';
+import '../../../com/iinspectable.dart';
+
+/// {@category Class}
+/// {@category winrt}
+class ToastNotification extends IInspectable
+    implements
+        IToastNotification,
+        IToastNotification2,
+        IToastNotification3,
+        IToastNotification4,
+        IToastNotification6 {
+  ToastNotification.from(super.ptr);
+
+  static const _className = 'Windows.UI.Notifications.ToastNotification';
+
+  // IToastNotificationFactory methods
+  static ToastNotification CreateToastNotification(Pointer<COMObject> content) {
+    final activationFactory =
+        CreateActivationFactory(_className, IID_IToastNotificationFactory);
+
+    try {
+      final result = IToastNotificationFactory.from(activationFactory)
+          .CreateToastNotification(content);
+      return ToastNotification.from(result);
+    } finally {
+      free(activationFactory);
+    }
+  }
+
+  // IToastNotification methods
+  late final _iToastNotification =
+      IToastNotification.from(toInterface(IID_IToastNotification));
+
+  @override
+  Pointer<COMObject> get Content => _iToastNotification.Content;
+
+  @override
+  set ExpirationTime(Pointer<COMObject> value) =>
+      _iToastNotification.ExpirationTime = value;
+
+  @override
+  Pointer<COMObject> get ExpirationTime => _iToastNotification.ExpirationTime;
+
+  @override
+  int add_Dismissed(Pointer<NativeFunction<TypedEventHandler>> handler) =>
+      _iToastNotification.add_Dismissed(handler);
+
+  @override
+  void remove_Dismissed(int token) =>
+      _iToastNotification.remove_Dismissed(token);
+
+  @override
+  int add_Activated(Pointer<NativeFunction<TypedEventHandler>> handler) =>
+      _iToastNotification.add_Activated(handler);
+
+  @override
+  void remove_Activated(int token) =>
+      _iToastNotification.remove_Activated(token);
+
+  @override
+  int add_Failed(Pointer<NativeFunction<TypedEventHandler>> handler) =>
+      _iToastNotification.add_Failed(handler);
+
+  @override
+  void remove_Failed(int token) => _iToastNotification.remove_Failed(token);
+  // IToastNotification2 methods
+  late final _iToastNotification2 =
+      IToastNotification2.from(toInterface(IID_IToastNotification2));
+
+  @override
+  set Tag(String value) => _iToastNotification2.Tag = value;
+
+  @override
+  String get Tag => _iToastNotification2.Tag;
+
+  @override
+  set Group(String value) => _iToastNotification2.Group = value;
+
+  @override
+  String get Group => _iToastNotification2.Group;
+
+  @override
+  set SuppressPopup(bool value) => _iToastNotification2.SuppressPopup = value;
+
+  @override
+  bool get SuppressPopup => _iToastNotification2.SuppressPopup;
+  // IToastNotification3 methods
+  late final _iToastNotification3 =
+      IToastNotification3.from(toInterface(IID_IToastNotification3));
+
+  @override
+  int get NotificationMirroring => _iToastNotification3.NotificationMirroring;
+
+  @override
+  set NotificationMirroring(int value) =>
+      _iToastNotification3.NotificationMirroring = value;
+
+  @override
+  String get RemoteId => _iToastNotification3.RemoteId;
+
+  @override
+  set RemoteId(String value) => _iToastNotification3.RemoteId = value;
+  // IToastNotification4 methods
+  late final _iToastNotification4 =
+      IToastNotification4.from(toInterface(IID_IToastNotification4));
+
+  @override
+  Pointer<COMObject> get Data => _iToastNotification4.Data;
+
+  @override
+  set Data(Pointer<COMObject> value) => _iToastNotification4.Data = value;
+
+  @override
+  int get Priority => _iToastNotification4.Priority;
+
+  @override
+  set Priority(int value) => _iToastNotification4.Priority = value;
+  // IToastNotification6 methods
+  late final _iToastNotification6 =
+      IToastNotification6.from(toInterface(IID_IToastNotification6));
+
+  @override
+  bool get ExpiresOnReboot => _iToastNotification6.ExpiresOnReboot;
+
+  @override
+  set ExpiresOnReboot(bool value) =>
+      _iToastNotification6.ExpiresOnReboot = value;
+}
