55ea7de8ca58134a84f4a6f3abaacc1e1d4c08c8
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-06-26 21:29:55 +0300

    Fix tests

--- tool/generator/test/enum_projection_test.dart
+++ tool/generator/test/enum_projection_test.dart
@@ -5,35 +5,33 @@ import 'package:test/test.dart';
 import 'package:winmd/winmd.dart';
 
 void main() {
-  test('Enums projected correctly', () {
+  test('Enumerations projected correctly', () {
     final winTypeDef =
         MetadataStore.getMetadataForType('Windows.Foundation.AsyncStatus');
 
     final enumProjection = EnumProjection(winTypeDef!, 'AsyncStatus');
-    expect(enumProjection.hasFlagsAttribute, false);
-    expect(enumProjection.enumSetHelper, isEmpty);
-    expect(enumProjection.fields, equalsIgnoringWhitespace('''
+    expect(enumProjection.classDeclaration, equals('enum AsyncStatus {'));
+    expect(enumProjection.identifiers, equalsIgnoringWhitespace('''
       Started(0),\n
       Completed(1),\n
       Canceled(2),\n
       Error(3);'''));
   });
 
-  test(
-      'Enums that support a bitwise combination of its member values projected correctly',
-      () {
+  test('Flags Enumerations projected correctly', () {
     final winTypeDef =
         MetadataStore.getMetadataForType('Windows.Storage.FileAttributes');
 
-    final enumProjection = EnumProjection(winTypeDef!, 'FileAttributes');
-    expect(enumProjection.hasFlagsAttribute, true);
-    expect(enumProjection.enumSetHelper, isNotEmpty);
-    expect(enumProjection.fields, equalsIgnoringWhitespace('''
-      Normal(0),\n
-      ReadOnly(1),\n
-      Directory(16),\n
-      Archive(32),\n
-      Temporary(256),\n
-      LocallyIncomplete(512);'''));
+    final flagsEnumProjection =
+        FlagsEnumProjection(winTypeDef!, 'FileAttributes');
+    expect(flagsEnumProjection.classDeclaration,
+        equals('class FileAttributes extends FlagsEnum {'));
+    expect(flagsEnumProjection.identifiers, equalsIgnoringWhitespace('''
+      static const Normal = FileAttributes(0, name: 'Normal');\n
+      static const ReadOnly = FileAttributes(1, name: 'ReadOnly');\n
+      static const Directory = FileAttributes(16, name: 'Directory');\n
+      static const Archive = FileAttributes(32, name: 'Archive');\n
+      static const Temporary = FileAttributes(256, name: 'Temporary');\n
+      static const LocallyIncomplete = FileAttributes(512, name: 'LocallyIncomplete');'''));
   });
 }
