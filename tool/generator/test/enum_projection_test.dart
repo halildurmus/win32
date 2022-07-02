6bdb1d4bf02c74d3aa1b71a89a284a5410f0e7ce
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-07-02 14:45:18 +0300

    Fix tests

--- tool/generator/test/enum_projection_test.dart
+++ tool/generator/test/enum_projection_test.dart
@@ -13,10 +13,10 @@ void main() {
     expect(enumProjection.classDeclaration,
         equals('enum AsyncStatus implements WinRTEnum {'));
     expect(enumProjection.identifiers, equalsIgnoringWhitespace('''
-      Started(0),\n
-      Completed(1),\n
-      Canceled(2),\n
-      Error(3);'''));
+      started(0),\n
+      completed(1),\n
+      canceled(2),\n
+      error(3);'''));
   });
 
   test('Flags Enumerations projected correctly', () {
@@ -28,11 +28,11 @@ void main() {
     expect(flagsEnumProjection.classDeclaration,
         equals('class FileAttributes extends WinRTEnum {'));
     expect(flagsEnumProjection.identifiers, equalsIgnoringWhitespace('''
-      static const Normal = FileAttributes(0, name: 'Normal');\n
-      static const ReadOnly = FileAttributes(1, name: 'ReadOnly');\n
-      static const Directory = FileAttributes(16, name: 'Directory');\n
-      static const Archive = FileAttributes(32, name: 'Archive');\n
-      static const Temporary = FileAttributes(256, name: 'Temporary');\n
-      static const LocallyIncomplete = FileAttributes(512, name: 'LocallyIncomplete');'''));
+      static const normal = FileAttributes(0, name: 'normal');\n
+      static const readOnly = FileAttributes(1, name: 'readOnly');\n
+      static const directory = FileAttributes(16, name: 'directory');\n
+      static const archive = FileAttributes(32, name: 'archive');\n
+      static const temporary = FileAttributes(256, name: 'temporary');\n
+      static const locallyIncomplete = FileAttributes(512, name: 'locallyIncomplete');'''));
   });
 }
