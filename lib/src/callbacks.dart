1a166f8eef63abc0df900f17b8bf6ab38f5d573e
Tim Sneath <tim@sneath.org>
2022-11-25 13:55:40 -0800

    Add PVOID type

--- lib/src/callbacks.dart
+++ lib/src/callbacks.dart
@@ -45,7 +45,7 @@ typedef DrawStateProc = Int32 Function(
     IntPtr hdc, IntPtr lData, IntPtr wData, Int32 cx, Int32 cy);
 
 /// Application-defined callback function used with the EnumPageFiles function.
-typedef EnumPageFilesProc = BOOL Function(Pointer pContext,
+typedef EnumPageFilesProc = BOOL Function(PVOID pContext,
     Pointer<ENUM_PAGE_FILE_INFORMATION> pPageFileInfo, LPWSTR lpFilename);
 
 /// Application-defined callback function used with the EnumChildWindows
