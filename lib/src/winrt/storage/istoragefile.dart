8b52f53288bfb976471b45a8a00f9c1b8531b934
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2022-12-26 11:58:18 +0300

    Format istoragefile.dart

--- lib/src/winrt/storage/istoragefile.dart
+++ lib/src/winrt/storage/istoragefile.dart
@@ -144,7 +144,7 @@ class IStorageFile extends IInspectable implements IStorageItem {
     }
 
     final asyncAction = IAsyncAction.fromRawPointer(retValuePtr);
-   completeAsyncAction(asyncAction, completer);
+    completeAsyncAction(asyncAction, completer);
 
     return completer.future;
   }
