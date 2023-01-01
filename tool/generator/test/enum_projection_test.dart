d0f1d26ad8c6ae38d3130789520ac68b13779bd3
Halil İbrahim Durmuş <themachinepoi@gmail.com>
2023-01-01 13:40:34 +0300

    Update tests

--- tool/generator/test/enum_projection_test.dart
+++ tool/generator/test/enum_projection_test.dart
@@ -12,11 +12,34 @@ void main() {
     final enumProjection = EnumProjection(winTypeDef!, 'AsyncStatus');
     expect(enumProjection.classDeclaration,
         equals('enum AsyncStatus implements WinRTEnum {'));
-    expect(enumProjection.identifiers, equalsIgnoringWhitespace('''
-      started(0),\n
-      completed(1),\n
-      canceled(2),\n
-      error(3);'''));
+    expect(
+        enumProjection.identifiers,
+        orderedEquals([
+          'started(0)',
+          'completed(1)',
+          'canceled(2)',
+          'error(3)',
+        ]));
+  });
+
+  test('Acronyms in enum identifiers are converted to lowercase', () {
+    final winTypeDef = MetadataStore.getMetadataForType(
+        'Windows.Devices.Geolocation.PositionSource');
+
+    final enumProjection = EnumProjection(winTypeDef!, 'PositionSource');
+    expect(enumProjection.classDeclaration,
+        equals('enum PositionSource implements WinRTEnum {'));
+    expect(
+        enumProjection.identifiers,
+        orderedEquals([
+          'cellular(0)',
+          'satellite(1)',
+          'wifi(2)',
+          'ipAddress(3)',
+          'unknown(4)',
+          'default_(5)',
+          'obfuscated(6)'
+        ]));
   });
 
   test('Flags Enumerations projected correctly', () {
@@ -27,12 +50,15 @@ void main() {
         FlagsEnumProjection(winTypeDef!, 'FileAttributes');
     expect(flagsEnumProjection.classDeclaration,
         equals('class FileAttributes extends WinRTEnum {'));
-    expect(flagsEnumProjection.identifiers, equalsIgnoringWhitespace('''
-      static const normal = FileAttributes(0, name: 'normal');\n
-      static const readOnly = FileAttributes(1, name: 'readOnly');\n
-      static const directory = FileAttributes(16, name: 'directory');\n
-      static const archive = FileAttributes(32, name: 'archive');\n
-      static const temporary = FileAttributes(256, name: 'temporary');\n
-      static const locallyIncomplete = FileAttributes(512, name: 'locallyIncomplete');'''));
+    expect(
+        flagsEnumProjection.identifiers,
+        orderedEquals([
+          "static const normal = FileAttributes(0, name: 'normal');",
+          "static const readOnly = FileAttributes(1, name: 'readOnly');",
+          "static const directory = FileAttributes(16, name: 'directory');",
+          "static const archive = FileAttributes(32, name: 'archive');",
+          "static const temporary = FileAttributes(256, name: 'temporary');",
+          "static const locallyIncomplete = FileAttributes(512, name: 'locallyIncomplete');",
+        ]));
   });
 }
