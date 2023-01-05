2e2d16361ebb74bac6d97d5551d8f0fbb1570fab
Tim Sneath <tim@sneath.org>
2023-01-04 22:17:46 -0800

    Remove now-deleted example

--- tool/build.cmd
+++ tool/build.cmd
@@ -1,6 +1,5 @@
 @echo off
 echo Compiling Dart executables...
-call dart compile exe example\console.dart -o example\bin\console.exe
 call dart compile exe example\hello.dart -o example\bin\hello.exe
 call dart compile exe example\notepad\notepad.dart -o example\bin\dartnote.exe
 call dart compile exe example\tetris\main.dart -o example\bin\tetris.exe
