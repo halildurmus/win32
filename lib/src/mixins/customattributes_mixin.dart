9bbce1246a5a043a71081008718946c908eb04bd
Tim Sneath <tim@sneath.org>
2023-01-06 12:52:41 -0800

    Last bit of cleanup

--- lib/src/mixins/customattributes_mixin.dart
+++ lib/src/mixins/customattributes_mixin.dart
@@ -10,7 +10,7 @@ import '../type_aliases.dart';
 
 /// Represents an object that has custom (named) attributes associated with it.
 mixin CustomAttributesMixin on TokenObject {
-  final _customAttributes = <CustomAttribute>[];
+  late final customAttributes = _getCustomAttributes();
 
   /// Retrieve the string associated with a specific attribute name.
   ///
@@ -38,31 +38,30 @@ mixin CustomAttributesMixin on TokenObject {
   bool existsAttribute(String attrName) => findAttribute(attrName) != null;
 
   /// Enumerate all attributes that this object has.
-  List<CustomAttribute> get customAttributes {
-    if (_customAttributes.isEmpty) {
-      using((Arena arena) {
-        final phEnum = arena<HCORENUM>();
-        final rAttrs = arena<mdCustomAttribute>();
-        final pcAttrs = arena<ULONG>();
+  Iterable<CustomAttribute> _getCustomAttributes() {
+    final customAttributes = <CustomAttribute>[];
+    using((Arena arena) {
+      final phEnum = arena<HCORENUM>();
+      final rAttrs = arena<mdCustomAttribute>();
+      final pcAttrs = arena<ULONG>();
 
-        // Certain TokenObjects may not have a valid token (e.g. a return
-        // type has a token of 0). In this case, we return an empty set, since
-        // calling EnumCustomAttributes with a scope of 0 will return all
-        // attributes on all objects in the scope.
-        if (!isResolvedToken) return <CustomAttribute>[];
+      // Certain TokenObjects may not have a valid token (e.g. a return
+      // type has a token of 0). In this case, we return an empty set, since
+      // calling EnumCustomAttributes with a scope of 0 will return all
+      // attributes on all objects in the scope.
+      if (!isResolvedToken) return <CustomAttribute>[];
 
-        var hr =
-            reader.EnumCustomAttributes(phEnum, token, 0, rAttrs, 1, pcAttrs);
-        while (hr == S_OK) {
-          final attrToken = rAttrs.value;
+      var hr =
+          reader.EnumCustomAttributes(phEnum, token, 0, rAttrs, 1, pcAttrs);
+      while (hr == S_OK) {
+        final attrToken = rAttrs.value;
 
-          _customAttributes.add(CustomAttribute.fromToken(scope, attrToken));
-          hr =
-              reader.EnumCustomAttributes(phEnum, token, 0, rAttrs, 1, pcAttrs);
-        }
-        reader.CloseEnum(phEnum.value);
-      });
-    }
-    return _customAttributes;
+        customAttributes.add(CustomAttribute.fromToken(scope, attrToken));
+        hr = reader.EnumCustomAttributes(phEnum, token, 0, rAttrs, 1, pcAttrs);
+      }
+      reader.CloseEnum(phEnum.value);
+    });
+
+    return customAttributes;
   }
 }
