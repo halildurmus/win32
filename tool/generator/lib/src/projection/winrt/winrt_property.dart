a9068879c1a3032c0b64eec8e81fe3d0ca0a2a2b
Halil İbrahim Durmuş <durmushalil@proton.me>
2023-01-06 18:06:57 +0300

    Refactor get property

--- tool/generator/lib/src/projection/winrt/winrt_property.dart
+++ tool/generator/lib/src/projection/winrt/winrt_property.dart
@@ -7,34 +7,24 @@ abstract class WinRTPropertyProjection extends WinRTMethodProjection {
 
   /// Strip off all underscores, even if double underscores
   String get exposedMethodName {
-    // e.g. get__Languages, put__Data
+    final String formattedMethodName;
     if (method.name.startsWith('get__') || method.name.startsWith('put__')) {
       // e.g. get__Languages -> Languages, put__Data -> Data
-      final formattedMethodName = method.name.substring(5);
-
-      for (final acronym in acronyms) {
-        if (formattedMethodName.startsWith(acronym)) {
-          // e.g. IPInformation -> ipInformation
-          return safeIdentifierForString(acronym.toLowerCase() +
-              formattedMethodName.substring(acronym.length));
-        }
-      }
-
-      return safeIdentifierForString(formattedMethodName).toCamelCase();
+      formattedMethodName = method.name.substring(5);
     } else {
       // e.g. get_Size -> Size, put_Completed -> Completed
-      final formattedMethodName = method.name.substring(4);
-
-      for (final acronym in acronyms) {
-        if (formattedMethodName.startsWith(acronym)) {
-          // e.g. IPInformation -> ipInformation
-          return safeIdentifierForString(acronym.toLowerCase() +
-              formattedMethodName.substring(acronym.length));
-        }
-      }
-
-      return safeIdentifierForString(formattedMethodName).toCamelCase();
+      formattedMethodName = method.name.substring(4);
     }
+
+    for (final acronym in acronyms) {
+      if (formattedMethodName.startsWith(acronym)) {
+        // e.g. IPInformation -> ipInformation
+        return safeIdentifierForString(acronym.toLowerCase() +
+            formattedMethodName.substring(acronym.length));
+      }
+    }
+
+    return safeIdentifierForString(formattedMethodName).toCamelCase();
   }
 
   // WinRT properties always return an HRESULT
