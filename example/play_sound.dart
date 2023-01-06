3f8ef822f0d73968b8d724e5a9b5d1a74b410dc6
Tim Sneath <tim@sneath.org>
2023-01-06 15:07:22 -0800

    Revert

--- example/play_sound.dart
+++ example/play_sound.dart
@@ -12,7 +12,6 @@ import 'package:win32/win32.dart';
 
 void main() {
   const logonSound = r'C:\Windows\Media\Windows Logon.wav';
-  final pLogonSound = logonSound.toNativeUtf16();
 
   final file = File(logonSound).existsSync();
 
@@ -20,11 +19,12 @@ void main() {
     print('WAV file missing.');
     exit(1);
   } else {
-    final result = PlaySound(pLogonSound, NULL, SND_FILENAME | SND_SYNC);
+    final pszLogonSound = logonSound.toNativeUtf16();
+    final result = PlaySound(pszLogonSound, NULL, SND_FILENAME | SND_SYNC);
 
     if (result != TRUE) {
       print('Sound playback failed.');
     }
+    free(pszLogonSound);
   }
-  free(pLogonSound);
 }
