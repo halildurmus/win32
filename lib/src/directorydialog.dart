f7ce8762e334833fbdf70e289e4a022e2c004382
Tim Sneath <tim@sneath.org>
2023-04-29 12:53:04 -0700

    Tidy up directory picker

--- lib/src/directorydialog.dart
+++ lib/src/directorydialog.dart
@@ -8,9 +8,11 @@ import 'filedialog.dart';
 import 'place.dart';
 
 class DirectoryPicker extends FileDialog {
+  /// Returns a directory selected by the user using a common file open dialog.
+  /// If the user clicks Cancel, then this function instead returns null.
   Directory? getDirectory() {
     var didUserCancel = false;
-    late String path;
+    var path = '';
 
     var hr = CoInitializeEx(
       nullptr,
@@ -25,31 +27,25 @@ class DirectoryPicker extends FileDialog {
     if (FAILED(hr)) throw WindowsException(hr);
 
     var options = pfos.value;
-
     options |= FILEOPENDIALOGOPTIONS.FOS_PICKFOLDERS;
-
-    if (hidePinnedPlaces) {
-      options |= FILEOPENDIALOGOPTIONS.FOS_HIDEPINNEDPLACES;
-    }
-
-    if (fileMustExist) {
-      options |= FILEOPENDIALOGOPTIONS.FOS_PATHMUSTEXIST;
-    }
-
+    if (hidePinnedPlaces) options |= FILEOPENDIALOGOPTIONS.FOS_HIDEPINNEDPLACES;
+    if (fileMustExist) options |= FILEOPENDIALOGOPTIONS.FOS_PATHMUSTEXIST;
+    if (isDirectoryFixed) options |= FILEOPENDIALOGOPTIONS.FOS_NOCHANGEDIR;
     if (forceFileSystemItems) {
       options |= FILEOPENDIALOGOPTIONS.FOS_FORCEFILESYSTEM;
-    }
-
-    if (isDirectoryFixed) {
-      options |= FILEOPENDIALOGOPTIONS.FOS_NOCHANGEDIR;
     }
 
     hr = dialog.setOptions(options);
     if (FAILED(hr)) throw WindowsException(hr);
 
     if (title.isNotEmpty) {
-      hr = dialog.setTitle(TEXT(title));
-      if (FAILED(hr)) throw WindowsException(hr);
+      final pTitle = title.toNativeUtf16();
+      try {
+        hr = dialog.setTitle(pTitle);
+        if (FAILED(hr)) throw WindowsException(hr);
+      } finally {
+        free(pTitle);
+      }
     }
 
     for (final place in customPlaces) {
@@ -72,23 +68,29 @@ class DirectoryPicker extends FileDialog {
       }
     } else {
       final ppsi = calloc<Pointer<COMObject>>();
-      hr = dialog.getResult(ppsi);
-      if (FAILED(hr)) throw WindowsException(hr);
-
-      final item = IShellItem(ppsi.cast());
       final pathPtrPtr = calloc<IntPtr>();
-      hr = item.getDisplayName(SIGDN.SIGDN_FILESYSPATH, pathPtrPtr.cast());
-      if (FAILED(hr)) throw WindowsException(hr);
 
-      final pathPtr = Pointer<Utf16>.fromAddress(pathPtrPtr.value);
-      // MAX_PATH is the normal maximum, but if the process is set to support
-      // long file paths and the user selects a path with length > MAX_PATH
-      // characters, it could be longer. In this case, the file name will be
-      // truncated.
-      path = pathPtr.toDartString();
+      try {
+        hr = dialog.getResult(ppsi);
+        if (FAILED(hr)) throw WindowsException(hr);
 
-      hr = item.release();
-      if (FAILED(hr)) throw WindowsException(hr);
+        final item = IShellItem(ppsi.cast());
+        hr = item.getDisplayName(SIGDN.SIGDN_FILESYSPATH, pathPtrPtr.cast());
+        if (FAILED(hr)) throw WindowsException(hr);
+
+        final pathPtr = Pointer<Utf16>.fromAddress(pathPtrPtr.value);
+        // MAX_PATH is the normal maximum, but if the process is set to support
+        // long file paths and the user selects a path with length > MAX_PATH
+        // characters, it could be longer. In this case, the file name will be
+        // truncated.
+        path = pathPtr.toDartString();
+
+        hr = item.release();
+        if (FAILED(hr)) throw WindowsException(hr);
+      } finally {
+        free(ppsi);
+        free(pathPtrPtr);
+      }
     }
 
     hr = dialog.release();
