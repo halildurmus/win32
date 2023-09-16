1de3bf9f1ab9b2027da12c47ff97fdac7354c8d2
Halil Durmus <durmushalil@proton.me>
2023-09-16 12:24:30 +0300

    Add license header

--- lib/src/dart_project.dart
+++ lib/src/dart_project.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 /// A set of Flutter and Dart assets used to initialize a Flutter engine.
 class DartProject {
   /// The path to the assets directory.
