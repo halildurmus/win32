436e28554eb984fced12c0c7c1af021702cfa807
Halil Durmus <durmushalil@proton.me>
2023-09-18 13:24:19 +0300

    Add license header

--- example/inspector/lib/dpad.dart
+++ example/inspector/lib/dpad.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import 'dart:math';
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:win32_gamepad/win32_gamepad.dart';
--- example/inspector/lib/utils.dart
+++ example/inspector/lib/utils.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import 'package:win32_registry/win32_registry.dart';
 
 class WindowsSystemConfiguration {
