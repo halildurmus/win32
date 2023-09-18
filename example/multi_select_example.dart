3049159b01565119831c8f945e6cfb836e12338b
Halil Durmus <durmushalil@proton.me>
2023-09-18 14:42:21 +0300

    Add license header

--- example/multi_select_example.dart
+++ example/multi_select_example.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import 'package:filepicker_windows/filepicker_windows.dart';
 
 // Multi-select file open dialog box example
