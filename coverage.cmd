59173eb2b1b866a3b9a8cdfcc16005eae8570980
Halil Durmus <durmushalil@proton.me>
2023-09-18 20:29:03 +0300

    Update Build workflow

--- coverage.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-call dart test --coverage=coverage
-call dart pub global run coverage:format_coverage --lcov --check-ignore --in=coverage --out=lcov.info --packages=.dart_tool/package_config.json --report-on=lib
-echo Coverage generated!
