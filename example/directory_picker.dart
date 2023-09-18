35252d00a191b76d7fcc3c18861e1c38a01792b9
Halil Durmus <durmushalil@proton.me>
2023-09-18 17:14:31 +0300

    Add directory picker example

---
+++ example/directory_picker.dart
@@ -0,0 +1,16 @@
+// Copyright (c) 2023, Dart | Windows. Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+import 'package:filepicker_windows/filepicker_windows.dart';
+
+// Directory picker example
+
+void main() {
+  final file = DirectoryPicker()..title = 'Select a directory';
+
+  final result = file.getDirectory();
+  if (result != null) {
+    print(result.path);
+  }
+}
