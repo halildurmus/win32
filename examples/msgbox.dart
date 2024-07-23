9cae63cdfde1250f662b81b5c42678229a076b7f
Halil Durmus <durmushalil@proton.me>
2024-07-23 12:59:50 +0300

    Tweak msgbox example

--- examples/msgbox.dart
+++ examples/msgbox.dart
@@ -1,24 +1,29 @@
-// Demonstrates a MessageBox from the console
+// Demonstrates displaying a MessageBox with a warning icon and three buttons.
 
+import 'package:ffi/ffi.dart';
 import 'package:win32/win32.dart';
 
 void main() {
-  final message =
-      TEXT('This is not really an error, but we are pretending for the sake '
-          'of this test.\n\nResource error.\nDo you want to try again?');
-  final title = TEXT('Dart MessageBox Test');
+  final lpCaption = 'Dart MessageBox Demo'.toNativeUtf16();
+  final lpText = '''
+This is not really an error, but we are pretending for the sake of this demo.
+
+Resource error.
+Do you want to try again?
+'''
+      .toNativeUtf16();
 
   final result = MessageBox(
-      NULL,
-      message,
-      title,
-      MESSAGEBOX_STYLE.MB_ICONWARNING | // Warning
-          MESSAGEBOX_STYLE.MB_CANCELTRYCONTINUE | // Action button
-          MESSAGEBOX_STYLE.MB_DEFBUTTON2 // Second button is the default
-      );
+    NULL,
+    lpText,
+    lpCaption,
+    MESSAGEBOX_STYLE.MB_ICONWARNING | // Warning icon
+        MESSAGEBOX_STYLE.MB_CANCELTRYCONTINUE | // Action button
+        MESSAGEBOX_STYLE.MB_DEFBUTTON2, // Second button is the default
+  );
 
-  free(message);
-  free(title);
+  free(lpText);
+  free(lpCaption);
 
   switch (result) {
     case MESSAGEBOX_RESULT.IDCANCEL:
