845c29a5e7fec60a4273cb441a675f4397da5e56
Halil Durmus <durmushalil@proton.me>
2024-07-27 17:19:29 +0300

    ci: update  `tools/build.cmd` to build "Snake" example

--- tools/build.cmd
+++ tools/build.cmd
@@ -1,6 +1,8 @@
 @echo off
 echo Compiling Dart executables...
-call dart compile exe examples\hello.dart -o examples\bin\hello.exe
+
 call dart compile exe examples\notepad\notepad.dart -o examples\bin\dartnote.exe
 call dart compile exe examples\tetris\main.dart -o examples\bin\tetris.exe
+call dart compile exe examples\hello.dart -o examples\bin\hello.exe
+call dart compile exe examples\snake.dart -o examples\bin\snake.exe
 call dart compile exe examples\taskdialog.dart -o examples\bin\taskdialog.exe
