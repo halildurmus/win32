8944587c2a4ab60a56516213b3ebf8b8fd6a393e
Halil Durmus <durmushalil@proton.me>
2024-07-28 13:39:21 +0300

    chore: remove copyright notices

--- lib/src/dart_project.dart
+++ lib/src/dart_project.dart
@@ -1,7 +1,3 @@
-// Copyright (c) 2023, Halil Durmus. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 import 'package:path/path.dart' as p;
 
 /// A set of Flutter and Dart assets used to initialize a Flutter engine.
