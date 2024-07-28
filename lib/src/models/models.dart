12f15ea2895252a4322f47f5f6ea25f7ee82d2f1
Halil Durmus <durmushalil@proton.me>
2024-07-28 16:39:28 +0300

    chore: remove copyright notices

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,6 +1,2 @@
-// Copyright (c) 2023, Halil Durmus. Please see the AUTHORS file for details.
-// All rights reserved. Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 export 'clipboard_format.dart';
 export 'exception.dart';
