92deff3613803f69679f3fde1962b7dc44ef3d27
Halil Durmus <durmushalil@proton.me>
2025-04-28 02:09:35 +0300

    docs: add docs for `Uint8ListExtension`

--- lib/src/reader/extensions.dart
+++ lib/src/reader/extensions.dart
@@ -1,6 +1,13 @@
 import 'dart:typed_data';
 
+/// Extension methods on [Uint8List] to simplify common metadata reading
+/// operations.
 extension Uint8ListExtension on Uint8List {
+  /// Reads a null-terminated UTF-8 string starting at the given [offset].
+  ///
+  /// Characters are read sequentially until a null terminator is encountered.
+  ///
+  /// The resulting string is returned.
   String readString(int offset) {
     final buffer = StringBuffer();
     var i = 0;
@@ -14,17 +21,24 @@ extension Uint8ListExtension on Uint8List {
     }
   }
 
+  /// Reads an unsigned 8-bit integer at the given [offset].
   @pragma('vm:prefer-inline')
   int readUint8(int offset) => this[offset];
 
+  /// Reads an unsigned 16-bit integer at the given [offset], assuming
+  /// little-endian encoding.
   @pragma('vm:prefer-inline')
   int readUint16(int offset) =>
       buffer.asByteData().getUint16(offset, Endian.little);
 
+  /// Reads an unsigned 32-bit integer at the given [offset], assuming
+  /// little-endian encoding.
   @pragma('vm:prefer-inline')
   int readUint32(int offset) =>
       buffer.asByteData().getUint32(offset, Endian.little);
 
+  /// Reads an unsigned 64-bit integer at the given [offset], assuming
+  /// little-endian encoding.
   @pragma('vm:prefer-inline')
   int readUint64(int offset) =>
       buffer.asByteData().getUint64(offset, Endian.little);
