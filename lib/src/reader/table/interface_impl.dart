dc1eb59386e216e780f2186ecabc3847b8eb0ae3
Halil Durmus <durmushalil@proton.me>
2025-04-28 15:29:23 +0300

    docs: improve docs of `InterfaceImpl`

--- lib/src/reader/table/interface_impl.dart
+++ lib/src/reader/table/interface_impl.dart
@@ -6,21 +6,27 @@ import '../metadata_table.dart';
 import '../row.dart';
 import 'type_def.dart';
 
-/// Records the interfaces a type implements explicitly.
+/// Represents a row in the `InterfaceImpl` metadata table, describing an
+/// interface that a type explicitly implements.
 ///
-/// The table has the following columns:
-///  - Class (TypeDef Index)
-///  - Interface (TypeDefOrRef Coded Index)
+/// The fields are populated by interpreting the binary metadata as specified in
+/// ECMA-335 `§II.22.23`.
 ///
-/// The table is defined in ECMA-335 `§II.22.23`.
+/// The `InterfaceImpl` table has the following columns:
+///  - **Class** (TypeDef Index)
+///  - **Interface** (TypeDefOrRef Coded Index)
 final class InterfaceImpl extends Row with HasCustomAttributes {
   InterfaceImpl(super.metadataIndex, super.readerIndex, super.position);
 
   @override
   MetadataTable get table => MetadataTable.interfaceImpl;
 
+  /// The type that implements the interface.
   late final class$ = readRow<TypeDef>(0);
 
+  /// Returns the interface type implemented by [class$].
+  ///
+  /// Optionally, [generics] can be passed to substitute any generic parameters..
   MetadataType interface([List<MetadataType> generics = const []]) =>
       decode<TypeDefOrRef>(1).type(generics);
 
