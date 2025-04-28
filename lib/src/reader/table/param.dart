c821ad8abceed99c58d7a882e66a44d1bc9898b2
Halil Durmus <durmushalil@proton.me>
2025-04-28 15:47:34 +0300

    docs: improve docs of `Param`

--- lib/src/reader/table/param.dart
+++ lib/src/reader/table/param.dart
@@ -6,26 +6,34 @@ import '../metadata_table.dart';
 import '../row.dart';
 import 'constant.dart';
 
-/// Records the parameters of a method or property.
+/// Represents a row in the `Param` metadata table, describing a parameter of a
+/// method or property.
 ///
-/// The table has the following columns:
-///  - Flags (2-byte bitmask of ParamAttributes)
-///  - Sequence (2-byte constant)
-///  - Name (String Heap Index)
+/// The fields are populated by interpreting the binary metadata as specified in
+/// ECMA-335 `§II.22.33`.
 ///
-/// The table is defined in ECMA-335 `§II.22.33`.
+/// The `Param` table has the following columns:
+///  - **Flags** (2-byte bitmask of ParamAttributes)
+///  - **Sequence** (2-byte constant)
+///  - **Name** (String Heap Index)
 final class Param extends Row with HasCustomAttributes {
   Param(super.metadataIndex, super.readerIndex, super.position);
 
   @override
   MetadataTable get table => MetadataTable.param;
 
+  /// Param attributes that indicates various attributes of the parameter,
+  /// such as whether it is optional, has a default value, etc.
   late final flags = ParamAttributes(readUint(0));
 
+  /// The zero-based index (sequence number) of the parameter in the method or
+  /// property.
   late final sequence = readUint(1);
 
+  /// The name of the parameter.
   late final name = readString(2);
 
+  /// The constant associated with the parameter, if any.
   late final constant = getEqualRange<Constant>(
     1,
     HasConstantParam(this).encode(),
