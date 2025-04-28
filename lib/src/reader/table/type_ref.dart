ec0f1ed2caee3c1b54996ad8a8b3725e69930576
Halil Durmus <durmushalil@proton.me>
2025-04-28 15:55:11 +0300

    docs: improve docs of `TypeRef`

--- lib/src/reader/table/type_ref.dart
+++ lib/src/reader/table/type_ref.dart
@@ -4,24 +4,30 @@ import '../metadata_index.dart';
 import '../metadata_table.dart';
 import '../row.dart';
 
-/// Contains the definitions of all types referenced in the assembly.
+/// Represents a row in the `TypeRef` metadata table, describing a reference to
+/// a type.
 ///
-/// The table has the following columns:
-///  - ResolutionScope (ResolutionScope Coded Index)
-///  - TypeName (String Heap Index)
-///  - TypeNamespace (String Heap Index)
+/// The fields are populated by interpreting the binary metadata as specified in
+/// ECMA-335 `§II.22.38`.
 ///
-/// The table is defined in ECMA-335 `§II.22.38`.
+/// The `TypeRef` table has the following columns:
+///  - **ResolutionScope** (ResolutionScope Coded Index)
+///  - **TypeName** (String Heap Index)
+///  - **TypeNamespace** (String Heap Index)
 final class TypeRef extends Row with HasCustomAttributes {
   TypeRef(super.metadataIndex, super.readerIndex, super.position);
 
   @override
   MetadataTable get table => MetadataTable.typeRef;
 
+  /// The resolution scope of the referenced type, indicating the context or
+  /// scope in which the type is defined.
   late final scope = decode<ResolutionScope>(0);
 
+  /// The name of the referenced type.
   late final name = readString(1);
 
+  /// The namespace of the referenced type.
   late final namespace = readString(2);
 
   @override
