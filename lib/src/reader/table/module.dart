742f46501794a24a644ab87dcde09a7f5906c18c
Halil Durmus <durmushalil@proton.me>
2025-04-28 15:40:47 +0300

    docs: improve docs of `Module`

--- lib/src/reader/table/module.dart
+++ lib/src/reader/table/module.dart
@@ -2,24 +2,29 @@ import '../metadata_index.dart';
 import '../metadata_table.dart';
 import '../row.dart';
 
-/// Contains a list of modules that are part of the assembly.
+/// Represents a row in the `Module` metadata table, describing a module
+/// associated with the assembly.
 ///
-/// The table has the following columns:
-///  - Generation (2-byte value, reserved, MBZ)
-///  - Name (String Heap Index)
-///  - Mvid (Module Version ID) (GUID Heap Index)
-///  - EncId (GUID Heap Index, reserved, MBZ)
-///  - EncBaseId (GUID Heap Index, reserved, MBZ)
+/// The fields are populated by interpreting the binary metadata as specified in
+/// ECMA-335 `§II.22.30`.
 ///
-/// The table is defined in ECMA-335 `§II.22.30`.
+/// The `Module` table has the following columns:
+///  - **Generation** (2-byte value, reserved, MBZ)
+///  - **Name** (String Heap Index)
+///  - **Mvid** (Module Version ID) (GUID Heap Index)
+///  - **EncId** (GUID Heap Index, reserved, MBZ)
+///  - **EncBaseId** (GUID Heap Index, reserved, MBZ)
 final class Module extends Row {
   Module(super.metadataIndex, super.readerIndex, super.position);
 
   @override
   MetadataTable get table => MetadataTable.module;
 
+  /// The name of the module.
   late final name = readString(1);
 
+  /// The Module Version ID (MVID), a GUID that uniquely identifies the version
+  /// of the module.
   late final mvid = readGuid(2);
 
   @override
