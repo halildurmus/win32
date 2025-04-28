29cbd5bc3b70a51be2112837934078995abdee05
Halil Durmus <durmushalil@proton.me>
2025-04-28 22:32:20 +0300

    feat: create`mdmerge` library

---
+++ lib/mdmerge.dart
@@ -0,0 +1,7 @@
+/// This library provides the `mdmerge` function, which reads and consolidates
+/// type definitions, methods, fields, attributes, and related metadata from
+/// multiple `.winmd` files or directories containing `.winmd` files, producing
+/// a single, unified metadata file.
+library;
+
+export 'src/mdmerge.dart';
