a142bb64b9e376096ddd9f2baabfa0d8053e4c4a
Halil Durmus <durmushalil@proton.me>
2025-04-28 15:16:21 +0300

    docs: improve docs of `Field`

--- lib/src/reader/table/field.dart
+++ lib/src/reader/table/field.dart
@@ -8,30 +8,34 @@ import 'constant.dart';
 import 'field_layout.dart';
 import 'type_def.dart';
 
-/// A field.
+/// Represents a row in the `Field` metadata table, describing a field within a
+/// type.
 ///
-/// Fields are typed memory locations that store the data of a program.
+/// The fields are populated by interpreting the binary metadata as specified in
+/// ECMA-335 `§II.22.15`.
 ///
-/// The table has the following columns:
-///  - Flags (2-byte bitmask of FieldAttributes)
-///  - Name (String Heap Index)
-///  - Signature (Blob Heap Index)
-///
-/// The table is defined in ECMA-335 `§II.22.15`.
+/// The `Field` table has the following columns:
+///  - **Flags** (2-byte bitmask of FieldAttributes)
+///  - **Name** (String Heap Index)
+///  - **Signature** (Blob Heap Index)
 final class Field extends Row with HasCustomAttributes {
   Field(super.metadataIndex, super.readerIndex, super.position);
 
   @override
   MetadataTable get table => MetadataTable.field;
 
+  /// Field attributes describing visibility, special semantics, and other
+  /// modifiers.
   late final flags = FieldAttributes(readUint(0));
 
-  /// Returns the visibility of the field (public, private, etc.).
+  /// The access level (visibility) of the field.
   late final fieldAccess =
       FieldAccess.values[flags & FieldAttributes.fieldAccessMask];
 
+  /// The name of the field.
   late final name = readString(1);
 
+  /// The metadata type of the field.
   late final type = () {
     final blob = readBlob(2);
     final prolog = blob.readUint8();
@@ -39,18 +43,21 @@ final class Field extends Row with HasCustomAttributes {
     return blob.readTypeSignature();
   }();
 
+  /// The constant value associated with the field, if any.
   late final constant = getEqualRange<Constant>(
     1,
     HasConstantField(this).encode(),
   ).firstOrNull;
 
+  /// The layout information for the field, if any.
   late final layout = getEqualRange<FieldLayout>(1, position + 1).firstOrNull;
 
+  /// The [TypeDef] that owns this field.
   late final parent = parentRow<TypeDef>(4);
 
   @override
   String toString() =>
-      'Field(name: $name, signature: $type'
+      'Field(name: $name, type: $type'
       '${constant != null ? ', constant: $constant' : ''})';
 }
 
