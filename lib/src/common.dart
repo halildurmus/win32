34ad16c38c67810683261d4288b7d04742488f6c
Halil Durmus <durmushalil@proton.me>
2025-04-29 02:50:35 +0300

    docs: add docs for `MetadataStream` and `Table` extension types

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -32,52 +32,142 @@ final class MetadataHeader extends Struct {
   external int streams;
 }
 
+/// Represents a metadata stream name within a .NET metadata file.
 extension type const MetadataStream._(String _) implements String {
+  /// The `#Blob` stream.
   static const blob = MetadataStream._('#Blob');
+
+  /// The `#GUID` stream.
   static const guid = MetadataStream._('#GUID');
+
+  /// The `#Strings` stream.
   static const string = MetadataStream._('#Strings');
+
+  /// The `#~` stream.
   static const table = MetadataStream._('#~');
+
+  /// The `#US` stream.
   static const userString = MetadataStream._('#US');
 }
 
+/// Represents a metadata table identifier within a .NET metadata file.
+///
+/// Metadata tables store structured metadata records, such as types, methods,
+/// parameters, and assemblies. Each table has a unique numeric identifier
+/// (sometimes called a "table number") defined by the ECMA-335 standard.
 extension type const Table._(int number) implements int {
+  /// The `Module` table.
   static const module = Table._(0x00);
+
+  /// The `TypeRef` table.
   static const typeRef = Table._(0x01);
+
+  /// The `TypeDef` table.
   static const typeDef = Table._(0x02);
+
+  /// The `Field` table.
   static const field = Table._(0x04);
+
+  /// The `MethodDef` table.
   static const methodDef = Table._(0x06);
+
+  /// The `Param` table.
   static const param = Table._(0x08);
+
+  /// The `InterfaceImpl` table.
   static const interfaceImpl = Table._(0x09);
+
+  /// The `MemberRef` table.
   static const memberRef = Table._(0x0A);
+
+  /// The `Constant` table.
   static const constant = Table._(0x0B);
+
+  /// The `CustomAttribute` table.
   static const customAttribute = Table._(0x0C);
+
+  /// The `FieldMarshal` table.
   static const fieldMarshal = Table._(0x0D);
+
+  /// The `DeclSecurity` table.
   static const declSecurity = Table._(0x0E);
+
+  /// The `ClassLayout` table.
   static const classLayout = Table._(0x0F);
+
+  /// The `FieldLayout` table.
   static const fieldLayout = Table._(0x10);
+
+  /// The `StandAloneSig` table.
   static const standAloneSig = Table._(0x11);
+
+  /// The `EventMap` table.
   static const eventMap = Table._(0x12);
+
+  /// The `Event` table.
   static const event = Table._(0x14);
+
+  /// The `PropertyMap` table.
   static const propertyMap = Table._(0x15);
+
+  /// The `Property` table.
   static const property = Table._(0x17);
+
+  /// The `MethodSemantics` table.
   static const methodSemantics = Table._(0x18);
+
+  /// The `MethodImpl` table.
   static const methodImpl = Table._(0x19);
+
+  /// The `ModuleRef` table.
   static const moduleRef = Table._(0x1A);
+
+  /// The `TypeSpec` table.
   static const typeSpec = Table._(0x1B);
+
+  /// The `ImplMap` table.
   static const implMap = Table._(0x1C);
+
+  /// The `FieldRVA` table.
   static const fieldRVA = Table._(0x1D);
+
+  /// The `Assembly` table.
   static const assembly = Table._(0x20);
+
+  /// The `AssemblyProcessor` table.
   static const assemblyProcessor = Table._(0x21);
+
+  /// The `AssemblyOS` table.
   static const assemblyOS = Table._(0x22);
+
+  /// The `AssemblyRef` table.
   static const assemblyRef = Table._(0x23);
+
+  /// The `AssemblyRefProcessor` table.
   static const assemblyRefProcessor = Table._(0x24);
+
+  /// The `AssemblyRefOS` table.
   static const assemblyRefOS = Table._(0x25);
+
+  /// The `File` table.
   static const file = Table._(0x26);
+
+  /// The `ExportedType` table.
   static const exportedType = Table._(0x27);
+
+  /// The `ManifestResource` table.
   static const manifestResource = Table._(0x28);
+
+  /// The `NestedClass` table.
   static const nestedClass = Table._(0x29);
+
+  /// The `GenericParam` table.
   static const genericParam = Table._(0x2A);
+
+  /// The `MethodSpec` table.
   static const methodSpec = Table._(0x2B);
+
+  /// The `GenericParamConstraint` table.
   static const genericParamConstraint = Table._(0x2C);
 }
 
